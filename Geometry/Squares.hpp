84077bc208aae68a7b2e5e53ff62dc91f7f72c9b
Rein Halbersma <rhalbersma@gmail.com>
2011-03-07 22:25:02 +0100

    small update to Rotate coordinates/squares

--- Geometry/Squares.hpp
+++ Geometry/Squares.hpp
@@ -14,21 +14,36 @@ struct Rotate<In, Angles::D000>
 template<typename In>
 struct Rotate<In, Angles::D270>
 {
-        typedef Grid<In::WIDTH, In::HEIGHT, (In::WIDTH % 2) ^ !In::PARITY> Out;
+        typedef Grid
+        <
+                In::WIDTH, 
+                In::HEIGHT, 
+                (In::WIDTH % 2) ^ !In::PARITY
+        > Out;
 };
 
 // rotate 90 degree left
 template<typename In>
 struct Rotate<In, Angles::D090>
 {
-        typedef Grid<In::WIDTH, In::HEIGHT, (In::HEIGHT % 2) ^ !In::PARITY> Out;
+        typedef Grid
+        <
+                In::WIDTH, 
+                In::HEIGHT, 
+                (In::HEIGHT % 2) ^ !In::PARITY
+        > Out;
 };
 
 // rotate 180 degrees
 template<typename In>
 struct Rotate<In, Angles::D180>
 {
-        typedef Grid<In::HEIGHT, In::WIDTH, (In::HEIGHT % 2) ^ (In::WIDTH % 2) ^ In::PARITY> Out;
+        typedef Grid
+        <
+                In::HEIGHT, 
+                In::WIDTH, 
+                (In::HEIGHT % 2) ^ (In::WIDTH % 2) ^ In::PARITY
+        > Out;
 };
 
 }       // namespace Squares
