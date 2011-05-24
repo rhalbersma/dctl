f0a20b213992eb26e54b45791c02e362cf7cb69c
Rein Halbersma <rhalbersma@gmail.com>
2011-05-24 21:57:23 +0200

    added testing of class Value in namespace Tree::Search moved all tests into their respective namespaces

---
+++ Source/Tree/Search/Value.cpp
@@ -0,0 +1,73 @@
+#include "../../../../Library/Source/Tree/Search/Value.h"
+#include "../../../../Library/Source/Utilities/Ply.h"
+#include "gtest/gtest.h"
+
+namespace Tree {
+namespace Search {
+namespace Value {
+
+TEST(Value, IsFiniteLoss)
+{
+        for (size_t i = 0; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, is_finite(loss(2 * i)));
+}
+
+TEST(Value, IsNegativeLoss)
+{
+        for (size_t i = 0; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, loss(2 * i) < 0);
+}
+
+TEST(Value, IsLoss)
+{
+        size_t i = 0;
+        for (; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, is_loss(loss(2 * i)));
+        EXPECT_EQ(false, is_loss(loss(2 * i)));
+}
+
+TEST(Value, IsFiniteWin)
+{
+        for (size_t i = 0; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, is_finite(win(2 * i + 1)));
+}
+
+TEST(Value, IsPositiveWin)
+{
+        for (size_t i = 0; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, win(2 * i + 1) > 0);
+}
+
+TEST(Value, IsWin)
+{
+        size_t i = 0;
+        for (; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, is_win(win(2 * i + 1)));
+        EXPECT_EQ(false, is_win(win(2 * i + 1)));
+}
+
+TEST(Value, IsWinEqualMinusLoss)
+{
+        for (size_t i = 0; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, win(2 * i + 1) == -loss(2 * i));
+}
+
+TEST(Value, StretchLoss)
+{
+        size_t i = 0;
+        EXPECT_EQ(true, is_infinite(stretch(loss(2 * i))));
+        for (i = 1; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, loss(2 * (i - 1)) == stretch(loss(2 * i)));
+}
+
+TEST(Value, SqueezeWin)
+{
+        size_t i = 0;
+        for (; i < GRAFTED_PLY_MAX / 2; ++i)
+                EXPECT_EQ(true, win(2 * (i + 1) + 1) == squeeze(win(2 * i + 1)));
+        EXPECT_EQ(false, is_win(squeeze(win(2 * i + 1))));
+}
+
+}       // namespace Value
+}       // namespace Search
+}       // namespace Tree
--- Source/Utilities/Bit.cpp
+++ Source/Utilities/Bit.cpp
@@ -2,23 +2,25 @@
 #include "../../../Library/Source/Utilities/IntegerTypes.h"
 #include "gtest/gtest.h"
 
+namespace Bit {
+
 TEST(Bit, IsZero)
 {
         BitBoard b = 0;
-        EXPECT_EQ(true,  Bit::is_zero(b));
-        EXPECT_EQ(false, Bit::is_single(b));
-        EXPECT_EQ(false, Bit::is_double(b));
-        EXPECT_EQ(false, Bit::is_multiple(b));
+        EXPECT_EQ(true,  is_zero(b));
+        EXPECT_EQ(false, is_single(b));
+        EXPECT_EQ(false, is_double(b));
+        EXPECT_EQ(false, is_multiple(b));
 }
 
 TEST(Bit, IsSingle)
 {
         for (size_t i = 0; i < 64; ++i) {
                 BitBoard b = BitBoard(1) << i;
-                EXPECT_EQ(false, Bit::is_zero(b));
-                EXPECT_EQ(true,  Bit::is_single(b));
-                EXPECT_EQ(false, Bit::is_double(b));
-                EXPECT_EQ(false, Bit::is_multiple(b));
+                EXPECT_EQ(false, is_zero(b));
+                EXPECT_EQ(true,  is_single(b));
+                EXPECT_EQ(false, is_double(b));
+                EXPECT_EQ(false, is_multiple(b));
         }
 }
 
@@ -28,12 +30,12 @@ TEST(Bit, IsDouble)
                 for (size_t j = 0; j < 64; ++j) {                                
                         BitBoard b = (BitBoard(1) << i) ^ (BitBoard(1) << j);
                         if (i == j) {
-                                EXPECT_EQ(true, Bit::is_zero(b));
+                                EXPECT_EQ(true, is_zero(b));
                         } else {
-                                EXPECT_EQ(false, Bit::is_zero(b));
-                                EXPECT_EQ(false, Bit::is_single(b));
-                                EXPECT_EQ(true,  Bit::is_double(b));
-                                EXPECT_EQ(true,  Bit::is_multiple(b));
+                                EXPECT_EQ(false, is_zero(b));
+                                EXPECT_EQ(false, is_single(b));
+                                EXPECT_EQ(true,  is_double(b));
+                                EXPECT_EQ(true,  is_multiple(b));
                         }
                 }
         }
@@ -46,58 +48,60 @@ TEST(Bit, IsMultiple)
                         BitBoard b = ((BitBoard(1) << i) - 1) << j;
                         switch (i) {
                         case 0:
-                                EXPECT_EQ(true, Bit::is_zero(b));
+                                EXPECT_EQ(true, is_zero(b));
                                 break;
                         case 1:
-                                EXPECT_EQ(true, Bit::is_single(b));
+                                EXPECT_EQ(true, is_single(b));
                                 break;
                         case 2:
-                                EXPECT_EQ(true, Bit::is_double(b));
-                                EXPECT_EQ(true, Bit::is_multiple(b));
+                                EXPECT_EQ(true, is_double(b));
+                                EXPECT_EQ(true, is_multiple(b));
                                 break;
                         default:                        
-                                EXPECT_EQ(false, Bit::is_zero(b));
-                                EXPECT_EQ(false, Bit::is_single(b));
-                                EXPECT_EQ(false, Bit::is_double(b));
-                                EXPECT_EQ(true,  Bit::is_multiple(b));
+                                EXPECT_EQ(false, is_zero(b));
+                                EXPECT_EQ(false, is_single(b));
+                                EXPECT_EQ(false, is_double(b));
+                                EXPECT_EQ(true,  is_multiple(b));
                         }
                 }
         }
-        EXPECT_EQ(true, Bit::is_multiple(BitBoard(~0)));
+        EXPECT_EQ(true, is_multiple(BitBoard(~0)));
 }
 
 TEST(Bit, Index)
 {
         for (size_t i = 0; i < 64; ++i) {
                 BitBoard b = BitBoard(1) << i;
-                EXPECT_EQ(i, Bit::index(b));
+                EXPECT_EQ(i, index(b));
         }
 }
 
 TEST(Bit, Count)
 {
-        EXPECT_EQ(0, Bit::count(0));
+        EXPECT_EQ(0, count(0));
 
         for (size_t i = 0; i < 64; ++i) {
                 BitBoard b = BitBoard(1) << i;
-                EXPECT_EQ(1, Bit::count(b));                
+                EXPECT_EQ(1, count(b));                
         }
 
         for (size_t i = 0; i < 64; ++i) {
                 for (size_t j = 0; j < 64; ++j) {                                
                         BitBoard b = (BitBoard(1) << i) ^ (BitBoard(1) << j);
                         if (i == j) {
-                                EXPECT_EQ(0, Bit::count(b));
+                                EXPECT_EQ(0, count(b));
                         } else {
-                                EXPECT_EQ(2, Bit::count(b));
+                                EXPECT_EQ(2, count(b));
                         }
                 }
         }
 
         for (size_t i = 0; i < 64; ++i) {
                 BitBoard b = (BitBoard(1) << i) - 1;
-                EXPECT_EQ(i, Bit::count(b));
+                EXPECT_EQ(i, count(b));
         }
 
-        EXPECT_EQ(64, Bit::count(BitBoard(~0)));
+        EXPECT_EQ(64, count(BitBoard(~0)));
 }
+
+}       // namespace Bit
