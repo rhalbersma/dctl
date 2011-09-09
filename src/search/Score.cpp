547245593eff7fd30e621eb84145bb821046ea80
Rein Halbersma <rhalbersma@gmail.com>
2011-09-09 21:23:25 +0200

    introduced tests for Spanish move generation

--- src/search/Score.cpp
+++ src/search/Score.cpp
@@ -1,4 +1,3 @@
-#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp> 
 #include "../../../DCTL/src/search/Score.h"
 #include "../../../DCTL/src/utils/Ply.h"
@@ -10,60 +9,60 @@ BOOST_AUTO_TEST_SUITE(TestScore)
 
 BOOST_AUTO_TEST_CASE(IsFiniteLoss)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(true, is_finite(loss_value(2 * i)));
 }
 
 BOOST_AUTO_TEST_CASE(IsNegativeLoss)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_LT(loss_value(2 * i), 0);
 }
 
 BOOST_AUTO_TEST_CASE(IsLoss)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(true, is_loss(loss_value(2 * i)));
-        BOOST_CHECK_EQUAL(false, is_loss(loss_value(MAX_GRAFTED_PLY)));
+        BOOST_CHECK_EQUAL(false, is_loss(loss_value(MAX_MATE_MOVES)));
 }
 
 BOOST_AUTO_TEST_CASE(IsFiniteWin)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(true, is_finite(win_value(2 * i + 1)));
 }
 
 BOOST_AUTO_TEST_CASE(IsPositiveWin)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_GT(win_value(2 * i + 1), 0);
 }
 
 BOOST_AUTO_TEST_CASE(IsWin)
 {        
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(true, is_win(win_value(2 * i + 1)));
-        BOOST_CHECK_EQUAL(false, is_win(win_value(MAX_GRAFTED_PLY + 1)));
+        BOOST_CHECK_EQUAL(false, is_win(win_value(MAX_MATE_MOVES + 1)));
 }
 
 BOOST_AUTO_TEST_CASE(IsWinEqualMinusLoss)
 {
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(win_value(2 * i + 1), -loss_value(2 * i));
 }
 
 BOOST_AUTO_TEST_CASE(StretchLoss)
 {        
         BOOST_CHECK_EQUAL(true, is_infinite(stretch(loss_value(0))));
-        for (auto i = 1; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 1; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(loss_value(2 * (i - 1)), stretch(loss_value(2 * i)));
 }
 
 BOOST_AUTO_TEST_CASE(SqueezeWin)
 {        
-        for (auto i = 0; i < MAX_GRAFTED_PLY / 2; ++i)
+        for (auto i = 0; i < MAX_MATE_MOVES / 2; ++i)
                 BOOST_CHECK_EQUAL(win_value(2 * (i + 1) + 1), squeeze(win_value(2 * i + 1)));
-        BOOST_CHECK_EQUAL(false, is_win(squeeze(win_value(MAX_GRAFTED_PLY + 1))));
+        BOOST_CHECK_EQUAL(false, is_win(squeeze(win_value(MAX_MATE_MOVES + 1))));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
