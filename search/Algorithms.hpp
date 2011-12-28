df095d3c6efceed5072e968519accfac98cc89fc
rhalbersma <rhalbersma@gmail.com>
2011-12-28 01:34:07 +0100

    added different search objectives (kingscourt, misere play, draw heuristics)

--- search/Algorithms.hpp
+++ search/Algorithms.hpp
@@ -34,6 +34,35 @@ int Root<Rules, Board, Objective>::iterative_deepening(const Position<Board>& p,
         return score;
 }
 
+// iterative deepening with no move ordering at the root
+template<typename Rules, typename Board, typename Objective>
+int Root<Rules, Board, Objective>::proof_verify(const Position<Board>& p, int depth)
+{
+        auto score = -infinity();                
+
+        Variation pv;       
+        Timer timer;
+        announce(p, depth);
+
+        // find proof-candidate
+        int i;
+        for (i = 1; !is_mate(score) && i <= depth; i += ROOT_ID_INCREMENT) {
+                statistics_.reset();
+                score = pvs<PV>(p, -infinity(), infinity(), i, 0, pv);
+                insert_pv(p, pv.sequence(), score);
+                timer.split();
+                report(i, score, timer, p, pv.sequence());
+        }
+
+        std::cout << "Found proof-candidate, verifying without transposition cutoffs" << "\n\n";
+        statistics_.reset();
+        score = verify<PV>(p, -infinity(), infinity(), i - 1, 0, pv);
+        timer.split();
+        report(i - 1, score, timer, p, pv.sequence());
+
+        return score;
+}
+
 // principal variation search (PVS)
 template<typename Rules, typename Board, typename Objective> template<int NodeType>
 int Root<Rules, Board, Objective>::pvs(
@@ -54,10 +83,7 @@ int Root<Rules, Board, Objective>::pvs(
 
         // alpha < beta implies alpha <= beta - 1, 
         // with the strict inequality if and only if is_pv(NodeType)
-        BOOST_ASSERT(
-                ( is_pv(NodeType) && alpha <  beta - 1) ||
-                (!is_pv(NodeType) && alpha == beta - 1)
-        );
+        BOOST_ASSERT(is_pv(NodeType) == (alpha <  beta - 1));
 
         // alpha < beta <= +INF implies alpha <= win_min 
         // with equality, any finite score will fail low
@@ -70,16 +96,14 @@ int Root<Rules, Board, Objective>::pvs(
                 return beta;
 
         // terminal positions
-        const auto terminal_value = 
-                                              is_draw<Rules>(p)?  draw_value() : 
-                 !Successor<successor::Legal, Rules>::detect(p)?  loss_min()   : 
-                                                                  -infinity()
-        ;
-        if (is_finite(terminal_value) && depth > 0) {
-                const auto type = Bound::type(terminal_value, alpha, beta);
-                TT.insert(p, Transposition(terminal_value, type, depth, Transposition::no_move()));
-                if (type == Bound::exact)
-                        refutation.clear();
+        const auto terminal_value = Objective::value<Rules>(p);
+        if (is_finite(terminal_value)) {
+                if (depth > 0) {
+                        const auto type = Bound::type(terminal_value, alpha, beta);
+                        TT.insert(p, Transposition(terminal_value, type, depth, Transposition::no_move()));
+                        if (type == Bound::exact)
+                                refutation.clear();
+                }
                 return terminal_value;
         }
 
@@ -164,6 +188,143 @@ int Root<Rules, Board, Objective>::pvs(
 
         const auto type = Bound::type(best_value, original_alpha, beta);
         TT.insert(p, Transposition(best_value, type, depth, best_move));
+        return best_value;
+}
+
+// principal variation search (PVS)
+template<typename Rules, typename Board, typename Objective> template<int NodeType>
+int Root<Rules, Board, Objective>::verify(
+        const Position<Board>& p, int alpha, int beta, int depth, int ply, Variation& refutation
+)
+{
+        statistics_.update(ply);
+        
+        if (is_interrupted())
+                return alpha;
+
+        // -INF <= alpha < beta <= +INF
+        BOOST_ASSERT(
+                -infinity() <= alpha && 
+                      alpha <  beta  && 
+                      beta  <= infinity()
+        );
+
+        // alpha < beta implies alpha <= beta - 1, 
+        // with the strict inequality if and only if is_pv(NodeType)
+        BOOST_ASSERT(is_pv(NodeType) == (alpha <  beta - 1));
+
+        // alpha < beta <= +INF implies alpha <= win_min 
+        // with equality, any finite score will fail low
+        if (alpha == win_min())
+                return alpha;
+
+        // -INF <= alpha < beta implies loss_min <= beta
+        // with equality, any finite score will fail high
+        if (beta == loss_min())
+                return beta;
+
+        // terminal positions
+        const auto terminal_value = Objective::value<Rules>(p);
+        if (is_finite(terminal_value)) {
+                /*
+                if (depth > 0) {
+                        const auto type = Bound::type(terminal_value, alpha, beta);
+                        TT.insert(p, Transposition(terminal_value, type, depth, Transposition::no_move()));
+                        if (type == Bound::exact)
+                                refutation.clear();
+                }
+                */
+                return terminal_value;
+        }
+
+        // return evaluation in leaf nodes with valid moves
+        if (depth <= 0 || ply >= MAX_PLY)
+                return Evaluate<Rules>::evaluate(p);
+
+        auto TT_entry = TT.find(p);
+
+        /*
+        // TT cut-off for exact win/loss scores or for deep enough heuristic scores
+        if (TT_entry && (is_mate(TT_entry->value()) || TT_entry->depth() >= depth) && TT_entry->is_cutoff(alpha, beta))
+                return TT_entry->value();
+        */
+
+        // generate moves
+        Stack moves;
+        moves.reserve(32);
+        Successor<successor::Legal, Rules>::generate(p, moves);
+        BOOST_ASSERT(!moves.empty());
+
+        std::vector<int> move_order;
+        move_order.reserve(moves.size());                               // reserve enough room for all indices
+        iota_n(std::back_inserter(move_order), moves.size(), 0);        // generate indices [0, moves.size() - 1]
+
+        /*
+        // internal iterative deepening (IID)
+        if (!(TT_entry && TT_entry->has_move())) {
+                const auto IID_depth = is_pv(NodeType)? depth - 2 : depth / 2;
+                if (IID_depth > 0) {
+                        pvs<NodeType>(p, alpha, beta, IID_depth, ply, refutation);
+                        TT_entry = TT.find(p);
+                        BOOST_ASSERT(TT_entry);
+                }
+        }
+        */
+        // move ordering
+        if (TT_entry && TT_entry->has_move()) {
+                const auto TT_move = TT_entry->move() % moves.size();
+                std::swap(move_order[0], move_order[TT_move]);
+        }
+
+        // search moves
+        /*
+        const auto original_alpha = alpha;
+        */
+        auto best_value = -infinity();
+        auto best_move = Transposition::no_move();
+        int value;
+        int i;
+                
+        Variation continuation;
+        for (auto s = 0; s < static_cast<int>(move_order.size()); ++s) {
+                i = move_order[s];
+                // TODO: TT singular extension
+
+                // TODO: futility pruning
+
+                auto q = p;
+                q.attach(p);
+                q.template make<Rules>(moves[i]);
+
+                if (is_pv(NodeType) && s == 0)
+                        value = -squeeze(pvs<PV>(q, -stretch(beta), -stretch(alpha), depth - 1, ply + 1, continuation));
+                else {
+                        // TODO: late move reductions
+
+                        value = -squeeze(pvs<ZW>(q, -stretch(alpha + 1), -stretch(alpha), depth - 1, ply + 1, continuation));
+                        if (is_pv(NodeType) && alpha < value && value < beta)
+                                value = -squeeze(pvs<PV>(q, -stretch(beta), -stretch(alpha), depth - 1, ply + 1, continuation));
+                }
+
+                if (value > best_value) {
+                        best_value = value;
+                        best_move = i; 
+                        if (best_value >= beta)
+                                break;
+                        if (best_value > alpha) {
+                                alpha = best_value;
+                                refutation.set(best_move, continuation.sequence());
+                        }                    
+                }
+        }
+
+        // we must have found a best move with a finite value
+        BOOST_ASSERT(is_finite(best_value));
+        BOOST_ASSERT(best_move != Transposition::no_move());
+        /*
+        const auto type = Bound::type(best_value, original_alpha, beta);
+        TT.insert(p, Transposition(best_value, type, depth, best_move));
+        */
         return best_value;
 }
 
--- search/Objective.hpp
+++ search/Objective.hpp
@@ -1,51 +1,177 @@
 #pragma once
 #include "Score.hpp"
+#include "../bit/Bit.hpp"
+#include "../node/Position.hpp"
+#include "../successor/Selection.hpp"
+#include "../successor/Successor.hpp"
+#include <boost/config.hpp>             // BOOST_STATIC_CONSTANT
 
 namespace dctl {
 namespace search {
 
-template<typename Objective>
-struct MinimalLoss;
+/* first side to run out of moves, loses */
+struct NoMovesLeft;
 
-template<typename Objective>
-struct MinimalWin;
+/* first side to get a king, wins */
+struct Kingscourt;
 
 struct Regular;
-struct Suicide;
+struct Misere;
+
+struct FirstPlayerWin;
+struct SecondPlayerWin;
+struct HeuristicDraw;
+
+template
+<
+        typename TerminalDetection = NoMovesLeft,
+        typename TerminalScoring = Misere,
+        typename CycleScoring = HeuristicDraw
+>
+struct GameObjective
+{
+        template<typename Rules, typename Board>
+        static int value(const Position<Board>& p)
+        {
+                if (is_cycle(p)) 
+                        return cycle<CycleScoring>::value(p); 
+                                                             
+                if (is_terminal<TerminalDetection, Rules>()(p))
+                        return terminal<TerminalScoring>::value();
+
+                return -infinity(); 
+        }
+};
+
+template<typename, typename>
+struct is_terminal;
+
+template<typename Rules>
+struct is_terminal<NoMovesLeft, Rules>
+{
+        template<typename Board>
+        bool operator()(const Position<Board>& p) const
+        {
+                return !Successor<successor::Legal, Rules>::detect(p);
+        }
+};
+
+template<typename Rules>
+struct is_terminal<Kingscourt, Rules>
+{
+        template<typename Board>
+        bool operator()(const Position<Board>& p) const
+        {
+                return (
+                        (bit::count(active_kings(p)) - bit::count(passive_kings(p)) < 0) ||
+                        is_terminal<NoMovesLeft, Rules>()(p)
+                );
+        }
+};
+
+template<typename>
+struct terminal;
 
 template<>
-struct MinimalLoss<Regular>
+struct terminal<Regular>
 {
-        int operator()() const
+        static int value()
         {
-                return loss_value(0);
+                return loss_min();
         }
 };
 
 template<>
-struct MinimalWin<Regular>
+struct terminal<Misere>
 {
-        int operator()() const
+        static int value()
         {
-                return win_value(1);
+                return -terminal<Regular>::value();
+        }
+};
+
+template<typename Rules, typename Board> 
+bool is_draw(const Position<Board>& p)
+{
+        return (
+                is_cycle(p) || 
+                is_no_progress<Rules>(p)
+        );
+}
+
+template<typename Board> 
+bool is_cycle(const Position<Board>& p)
+{
+        // a cycle needs at least 4 reversible moves
+        if (p.reversible_moves() < 4)
+                return false;
+
+        // find the parent position at 4 ply above the current position
+        auto q = grand_parent(*grand_parent(p));
+
+        // compare the ancestor hash indices with the current hash index
+        for (auto i = 4; i <= p.reversible_moves(); i += 2) {
+                if (q->hash_index() == p.hash_index())
+                        return true;
+                q = grand_parent(*q);
+        }
+        return false;
+}       
+
+template<typename Rules, typename Board> 
+bool is_no_progress(const Position<Board>& p)
+{
+        // tag dispatching on restrictions on consecutive reversible moves        
+        return is_no_progress_dispatch<Rules>(
+                p, 
+                Int2Type<rules::is_restricted_reversible_moves<Rules>::value>()
+        );
+}
+
+// partial specialization for no restrictions on consecutive reversible moves
+template<typename Rules, typename Board> 
+bool is_no_progress_dispatch(const Position<Board>&, Int2Type<false>)
+{
+        return false;
+}
+
+// partial specialization for a maximum of consecutive reversible moves
+template<typename Rules, typename Board> 
+bool is_no_progress_dispatch(const Position<Board>& p, Int2Type<true>)
+{
+        return p.reversible_moves() >= rules::max_reversible_moves<Rules>::value;
+}
+
+template<typename>
+struct cycle;
+
+template<>
+struct cycle<HeuristicDraw>
+{
+        template<typename Board>
+        static int value(const Position<Board>& /* p */)
+        {
+                return draw_value();
         }
 };
 
 template<>
-struct MinimalLoss<Suicide>
+struct cycle<FirstPlayerWin>
 {
-        int operator()() const
+        template<typename Board>
+        static int value(const Position<Board>& p)
         {
-                return loss_value(1);
+                return (p.distance_to_root() % 2)? loss_min() : win_min();
         }
 };
 
 template<>
-struct MinimalWin<Suicide>
+struct cycle<SecondPlayerWin>
 {
-        int operator()() const
+        template<typename Board>
+        static int value(const Position<Board>& p)
         {
-                return win_value(0);
+                return -cycle<FirstPlayerWin>::value(p);
         }
 };
 
