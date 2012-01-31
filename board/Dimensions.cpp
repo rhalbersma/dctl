f94bea9dc277471c0467e489739e3541cedbe0da
rhalbersma <rhalbersma@gmail.com>
2012-01-31 23:35:18 +0100

    finalized MPL style group actions

--- board/Dimensions.cpp
+++ board/Dimensions.cpp
@@ -1,35 +1,33 @@
 #include <boost/test/unit_test.hpp> 
 #include <boost/test/test_case_template.hpp>
-
 #include <boost/mpl/list.hpp>                           // boost::mpl::list
-
-#include "Transform.hpp"
 #include "../../src/board/Dimensions.hpp"
+#include "../../src/board/Cyclic.hpp"
 #include "../../src/board/Group.hpp"
 
 namespace dctl {
-namespace board {
+namespace group {
 
 BOOST_AUTO_TEST_SUITE(TestDimensions)
 
 typedef boost::mpl::list<
-        Dimensions< 4,  4>,
-        Dimensions< 6,  6>,
-        Dimensions< 8,  8>,
-        Dimensions<10, 10>,
-        Dimensions<10,  8, true>,
-        Dimensions<11, 10, true>,
-        Dimensions<12, 10, true>
+        board::Dimensions< 4,  4>,
+        board::Dimensions< 6,  6>,
+        board::Dimensions< 8,  8>,
+        board::Dimensions<10, 10>,
+        board::Dimensions<10,  8, true>,
+        board::Dimensions<11, 10, true>,
+        board::Dimensions<12, 10, true>
 > DimensionsList;
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(RightAction, T, DimensionsList)
 {
-        is_right_action<T, group::C1>()();
-        is_right_action<T, group::C2>()();
-        is_right_action<T, group::C4>()();
+        BOOST_CHECK((is_right_action< T, C1 >::value));
+        BOOST_CHECK((is_right_action< T, C2 >::value));
+        BOOST_CHECK((is_right_action< T, C4 >::value));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
-}       // namespace board
+}       // namespace group
 }       // namespace dctl
