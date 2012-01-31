122860e1a4bff06dcec5fb0e616e309faa9e8418
Rein Halbersma <rhalbersma@gmail.com>
2012-01-31 23:35:18 +0100

    finalized MPL style group actions

--- board/Angle.cpp
+++ board/Angle.cpp
@@ -1,45 +1,39 @@
 #include <boost/test/unit_test.hpp> 
 #include <boost/test/test_case_template.hpp>
-#include <boost/mpl/list.hpp>                   // list
 #include <boost/mpl/quote.hpp>                  // quote1
-#include "Transform.hpp"                        // rotate, inverse, mirror_up, mirror_down
+#include "../../src/board/Angle.hpp"
+#include "../../src/board/Cyclic.hpp"
 #include "../../src/board/Group.hpp"
 
 namespace dctl {
+namespace group {
 
 BOOST_AUTO_TEST_SUITE(TestAngle)
 
-BOOST_AUTO_TEST_CASE(Axioms)
+BOOST_AUTO_TEST_CASE_TEMPLATE(RightAction, T, set<C8>::type)
 {
-        is_axioms<group::C1>()();
-        is_axioms<group::C2>()();
-        is_axioms<group::C4>()();
-        is_axioms<group::C8>()();
+        BOOST_CHECK((is_right_action< T, C1 >::value));
+        BOOST_CHECK((is_right_action< T, C2 >::value));
+        BOOST_CHECK((is_right_action< T, C4 >::value));
+        BOOST_CHECK((is_right_action< T, C8 >::value));
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(RightAction, T, group::set<group::C8>::type)
+BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentInverse, T, set<C8>::type)
 {
-        is_right_action<T, group::C1>()();
-        is_right_action<T, group::C2>()();
-        is_right_action<T, group::C4>()();
-        is_right_action<T, group::C8>()();
+        BOOST_CHECK((is_idempotent< boost::mpl::quote1< inverse >, T>::value));
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentInverse, T, group::set<group::C8>::type)
+BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentMirrorUp, T, set<C8>::type)
 {
-        BOOST_CHECK((is_idempotent< boost::mpl::quote1<inverse>, T>::value));
+        BOOST_CHECK((is_idempotent< boost::mpl::quote1< mirror_up >, T>::value));
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentMirrorUp, T, group::set<group::C8>::type)
+BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentMirrorDown, T, set<C8>::type)
 {
-        BOOST_CHECK((is_idempotent< boost::mpl::quote1<mirror_up>, T>::value));
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(IdemPotentMirrorDown, T, group::set<group::C8>::type)
-{
-        BOOST_CHECK((is_idempotent< boost::mpl::quote1<mirror_down>, T>::value));
+        BOOST_CHECK((is_idempotent< boost::mpl::quote1< mirror_down >, T>::value));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
 
+}       // namespace group
 }       // namespace dctl
---
+++ board/Cyclic.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp> 
+#include <boost/test/test_case_template.hpp>
+#include "../../src/board/Cyclic.hpp"
+#include "../../src/board/Group.hpp"
+
+namespace dctl {
+namespace group {
+
+BOOST_AUTO_TEST_SUITE(TestCyclicGroup)
+
+BOOST_AUTO_TEST_CASE(Axioms)
+{
+        BOOST_CHECK(is_axioms<C1>::value);
+        BOOST_CHECK(is_axioms<C2>::value);
+        BOOST_CHECK(is_axioms<C4>::value);
+        BOOST_CHECK(is_axioms<C8>::value);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}       // namespace group
+}       // namespace dctl
