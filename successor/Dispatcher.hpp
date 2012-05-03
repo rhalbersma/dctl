78f9abae420994e839480aa45e52ee2da2d34da3
rhalbersma <rhalbersma@gmail.com>
2012-05-03 23:45:06 +0200

    added local class template in Dispatcher.hpp to simulate C++11 template aliases

--- successor/Dispatcher.hpp
+++ successor/Dispatcher.hpp
@@ -32,37 +32,41 @@ struct Dispatcher
         // enforce static semantics
         private nonconstructible
 {
+private:
+        /*
+
+        TODO: use C++11 template aliases
+
+        template<bool Color, int Material> 
+        using Delegate = State<Color, Material, Selection, Rules, Board, Position>;
+
+        */
+
+        template<bool Color, int Material>
+        struct Delegate
+        :
+                State<Color, Material, Selection, Rules, Board, Position>
+        {};
+
+public:
         // typedefs
 
         typedef StateInterface< Position<Rules, Board> > const * BaseConstPointer;
 
         static BaseConstPointer select(int state /* partial intrinsic state encoding */ )
         {
-                /*
-
-                TODO: suppress outside template parameters using C++11 template aliases
-
-                template<bool Color, int Material> 
-                using Delegate = State<Color, Material, Selection, Rules, Board, Position>;
-
-                static const Delegate<Side::black, Material::none> black_none;
-                ...
-                static const Delegate<Side::black, Material::none> white_both;
-
-                */
-
                 // full intrinsic state representations
-                static const State<Side::black, Material::none, Selection, Rules, Board, Position> black_none;
-                static const State<Side::black, Material::pawn, Selection, Rules, Board, Position> black_pawn;
-                static const State<Side::black, Material::king, Selection, Rules, Board, Position> black_king;
-                static const State<Side::black, Material::both, Selection, Rules, Board, Position> black_both;
-                static const State<Side::white, Material::none, Selection, Rules, Board, Position> white_none;
-                static const State<Side::white, Material::pawn, Selection, Rules, Board, Position> white_pawn;
-                static const State<Side::white, Material::king, Selection, Rules, Board, Position> white_king;
-                static const State<Side::white, Material::both, Selection, Rules, Board, Position> white_both;
+                static const Delegate<Side::black, Material::none> black_none;
+                static const Delegate<Side::black, Material::pawn> black_pawn;
+                static const Delegate<Side::black, Material::king> black_king;
+                static const Delegate<Side::black, Material::both> black_both;
+                static const Delegate<Side::white, Material::none> white_none;
+                static const Delegate<Side::white, Material::pawn> white_pawn;
+                static const Delegate<Side::white, Material::king> white_king;
+                static const Delegate<Side::white, Material::both> white_both;
 
                 // "Meyers Singleton", Effective C++ 3rd ed., Item 4 (p. 31-32)
-                static std::array<BaseConstPointer, 8> const singleton_ = {
+                static BaseConstPointer const singleton_[] = {
                         &black_none, &black_pawn, &black_king, &black_both,
                         &white_none, &white_pawn, &white_king, &white_both
                 };
