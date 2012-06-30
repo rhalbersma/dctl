ef4a8cfc8017af9ff8538e9af0af3c696f8638f2
Rein Halbersma <rhalbersma@gmail.com>
2012-06-30 22:20:06 +0200

    added iterators and links for the game tree

---
+++ src/tree/iterator/Forward.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include <iterator>                     // iterator, forward_iterator_tag
+#include <type_traits>                  // is_base_of
+#include <boost/mpl/assert.hpp>         // BOOST_MPL_ASSERT
+#include <boost/operators.hpp>          // equality_comparable
+#include "Iterator_fwd.hpp"
+#include "../link/Single.hpp"
+
+namespace dctl {
+namespace tree {
+namespace iterator {
+
+template<typename T>
+struct Iterator<T, link::Single>
+:
+        public std::iterator<std::forward_iterator_tag, T>,
+        private boost::equality_comparable< Iterator<T, link::Single> >
+{
+        typedef link::Single link_type;
+
+        // structors
+
+        Iterator(link_type* it) 
+        :
+                link_(it) 
+        {}
+
+        // modifiers
+
+        Iterator& operator++() 
+        { 
+                link_ = link_->next_; 
+                return *this; 
+        }
+
+        Iterator operator++(int) 
+        {
+                auto tmp(*this); 
+                ++*this; 
+                return tmp;
+        }
+
+        // queries
+
+        reference operator*() const
+        {
+                return node()->value_;
+        }
+
+        pointer operator->() const
+        {
+                return &(node()->value_);
+        }
+
+        // predicates
+
+        bool operator==(Iterator const& other) const 
+        {
+                return link_ == other.link_;
+        }
+
+        // representation
+
+        link_type* link_;
+
+private:
+        typedef Node<T, link_type> node_type;
+
+        // implementation
+
+        node_type* node() const
+        {
+                BOOST_MPL_ASSERT((std::is_base_of<link_type, node_type>));
+                return static_cast<node_type*>(this);
+        }
+};
+
+// TODO: use C++11 template aliases
+template<T>
+struct Forward
+:
+        Iterator<T, link::Single>
+{};
+
+}       // namespace iterator
+}       // namespace tree
+}       // namespace dctl
---
+++ src/tree/tree.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <cstdint>                      // uint64_t
+
+namespace dctl {
+
+template
+<
+        typename T, 
+        typename Link
+>
+struct Node
+:
+        public Link
+{
+        // structors
+
+        Node(T const& v)
+        :
+                Link(),
+                value_(v)
+        {}
+
+        // representation
+
+        T value_;
+};
+
+}       // namespace dctl
