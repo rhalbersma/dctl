402f3aa927732408b89a8d6fe0b04ad8c978df4a
Rein Halbersma <Rein Halbersma@localhost>
2012-10-25 23:36:56 +0200

    clean up Fixture for walk tests to use ranged-for

--- test/walk/Fixture.hpp
+++ test/walk/Fixture.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include <cstddef>                              // size_t
-#include <iterator>                             // begin, end
-#include <boost/config.hpp>                     // BOOST_STATIC_CONSTANT
 #include <boost/test/unit_test.hpp>             // BOOST_CHECK_EQUAL
 #include <dctl/walk/Root.hpp>                   // Root
 #include <dctl/utility/IntegerTypes.hpp>        // NodeCount
@@ -22,15 +20,13 @@ public:
                 root_.resize_hash(1);
         }
 
-        BOOST_STATIC_CONSTANT(auto, R = 0);
-
         template<typename Position, std::size_t N>
         void run(Position const& p, NodeCount const (&leafs)[N])
         {
-                for (auto it = std::begin(leafs); it != std::prev(std::end(leafs), R); ++it) {
-                        root_.clear_hash();
-                        auto const depth = std::distance(std::begin(leafs), it) + 1;
-                        BOOST_CHECK_EQUAL(*it, root_.test(p, depth));
+                auto depth = 0;
+                for (auto const& node_count: leafs) {
+                        root_.clear_hash();                        
+                        BOOST_CHECK_EQUAL(node_count, root_.test(p, ++depth));
                 }
         }
 
