3751e17d0963baaf806aef39aaab28692461c0bd
Rein Halbersma <Rein Halbersma@localhost>
2012-11-02 11:27:02 +0100

    add Eclipse generated makefile

---
+++ Default/test/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../test/precompiled.cpp \
+../test/test_main.cpp 
+
+OBJS += \
+./test/precompiled.o \
+./test/test_main.o 
+
+CPP_DEPS += \
+./test/precompiled.d \
+./test/test_main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+test/%.o: ../test/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"C:\MinGW\include\boost" -I"C:\Users\Reinepein\workspace\dctl" -O3 -g -pedantic -pedantic-errors -Wall -Wextra -Werror -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Default/test/utility/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../test/utility/algorithm.cpp 
+
+OBJS += \
+./test/utility/algorithm.o 
+
+CPP_DEPS += \
+./test/utility/algorithm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+test/utility/%.o: ../test/utility/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"C:\MinGW\include\boost" -I"C:\Users\Reinepein\workspace\dctl" -O3 -g -pedantic -pedantic-errors -Wall -Wextra -Werror -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
