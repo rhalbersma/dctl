d6dbe2becf57fd0e96cd37c899a864dbe7595947
rhalbersma <rhalbersma@gmail.com>
2012-11-03 00:54:06 +0100

    add new MSVC++ features
    -variadic templates (incomplete support)
    -default function template arguments

--- dctl/utility/make_unique.hpp
+++ dctl/utility/make_unique.hpp
@@ -1,48 +1,31 @@
 #pragma once
 #include <memory>                       // unique_ptr
+#include <type_traits>
+#include <utility>
 
 namespace dctl {
+namespace detail {
 
-#ifndef _MSC_VER
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(std::false_type, Args&&... args)
+{
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(std::true_type, Args&&... args)
+{
+        static_assert(std::extent<T>::value == 0, "make_unique<T[N]>() is forbidden, please use make_unique<T[]>()");
+        typedef typename std::remove_extent<T>::type U;
+        return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
+}
+
+}       // namespace detail
 
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
 {
-    return (std::unique_ptr<T>(new T(std::forward<Args>(args)...)));
+        return detail::make_unique<T>(std::is_array<T>(), std::forward<Args>(args)...);
 }
-
-#else
-
-template
-<
-        typename T
->
-std::unique_ptr<T> make_unique()
-{
-        return (std::unique_ptr<T>(new T()));
-}
-
-template
-<
-        typename T,
-        typename Arg1
->
-std::unique_ptr<T> make_unique(Arg1&& a1)
-{
-        return (std::unique_ptr<T>(new T(std::forward<Arg1>(a1))));
-}
-
-template
-<
-        typename T,
-        typename Arg1,
-        typename Arg2
->
-std::unique_ptr<T> make_unique(Arg1&& a1, Arg2&& a2)
-{
-        return (std::unique_ptr<T>(new T(std::forward<Arg1>(a1), std::forward<Arg2>(a2))));
-}
-
-#endif
 
 }       // namespace dctl
