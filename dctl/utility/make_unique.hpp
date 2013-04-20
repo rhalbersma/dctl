f8e3ccc92603027007400e733e94687ccf7de847
rein <rein@Mirach-VirtualBox>
2013-04-20 11:15:20 +0200

    add N3588 make_unique proposal

--- dctl/utility/make_unique.hpp
+++ dctl/utility/make_unique.hpp
@@ -1,31 +1,57 @@
 #pragma once
-#include <memory>                       // unique_ptr
+
+/*
+
+Stephan T. Lavavej's make_unique proposal (N3588)
+http://isocpp.org/files/papers/n3588.txt
+
+*/
+
+#include <cstddef>
+#include <memory>
 #include <type_traits>
 #include <utility>
 
-namespace dctl {
-namespace detail {
+namespace std {
+    template<class T> struct _Never_true : false_type { };
 
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(std::false_type, Args&&... args)
-{
-        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    template<class T> struct _Unique_if {
+        typedef unique_ptr<T> _Single;
+    };
+
+    template<class T> struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Runtime;
+    };
+
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        static_assert(_Never_true<T>::value, "make_unique forbids T[N]. Please use T[].");
+    };
+
+    template<class T, class... Args> typename _Unique_if<T>::_Single make_unique(Args&&... args) {
+        return unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+
+    template<class T> typename _Unique_if<T>::_Single make_unique_default_init() {
+        return unique_ptr<T>(new T);
+    }
+
+    template<class T> typename _Unique_if<T>::_Runtime make_unique(size_t n) {
+        typedef typename remove_extent<T>::type U;
+        return unique_ptr<T>(new U[n]());
+    }
+
+    template<class T> typename _Unique_if<T>::_Runtime make_unique_default_init(size_t n) {
+        typedef typename remove_extent<T>::type U;
+        return unique_ptr<T>(new U[n]);
+    }
+
+    template<class T, class... Args> typename _Unique_if<T>::_Runtime make_unique_value_init(size_t n, Args&&... args) {
+        typedef typename remove_extent<T>::type U;
+        return unique_ptr<T>(new U[n]{ std::forward<Args>(args)... });
+    }
+
+    template<class T, class... Args> typename _Unique_if<T>::_Runtime make_unique_auto_size(Args&&... args) {
+        typedef typename remove_extent<T>::type U;
+        return unique_ptr<T>(new U[sizeof...(Args)]{ std::forward<Args>(args)... });
+    }
 }
-
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(std::true_type, Args&&... args)
-{
-        static_assert(std::extent<T>::value == 0, "make_unique<T[N]>() is forbidden, please use make_unique<T[]>()");
-        typedef typename std::remove_extent<T>::type U;
-        return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
-}
-
-}       // namespace detail
-
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args)
-{
-        return detail::make_unique<T>(std::is_array<T>(), std::forward<Args>(args)...);
-}
-
-}       // namespace dctl
