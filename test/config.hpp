d8465be2011fba393d5f24ab931cec00f0ac8d09
rhalbersma <rhalbersma@gmail.com>
2013-04-30 21:51:16 +0200

    streamlined CMake setup

--- test/config.hpp
+++ test/config.hpp
@@ -2,13 +2,13 @@
 
 #define SUCCESSOR_TEST                  1
 
-#define WALK_INITIAL_BOARD8x8           1
-#define WALK_INITIAL_RECTANGULAR        1
-#define WALK_INTERNATIONAL_FMJD         1
+#define WALK_INITIAL_BOARD8x8           0
+#define WALK_INITIAL_RECTANGULAR        0
+#define WALK_INTERNATIONAL_FMJD         0
 #define WALK_ITALIAN_GILBERT            0
 
-#define SEARCH_EGDB_FRISIAN             1
-#define SEARCH_EGDB_INTERNATIONAL       1
-#define SEARCH_EGDB_KILLER              1
+#define SEARCH_EGDB_FRISIAN             0
+#define SEARCH_EGDB_INTERNATIONAL       0
+#define SEARCH_EGDB_KILLER              0
 
-#define SEARCH_INITIAL_INTERNATIONAL    1
+#define SEARCH_INITIAL_INTERNATIONAL    0
