7f5fa7d616d96c66487fa7d263c0b37da351e018
rhalbersma <rhalbersma@gmail.com>
2013-05-13 20:50:15 +0200

    small fixes

--- test/test/config.hpp
+++ test/test/config.hpp
@@ -2,7 +2,7 @@
 
 #define SUCCESSOR_TEST                  1
 
-#define WALK_INITIAL_BOARD8x8           0
+#define WALK_INITIAL_BOARD8x8           1
 #define WALK_INITIAL_RECTANGULAR        0
 #define WALK_INTERNATIONAL_FMJD         0
 #define WALK_ITALIAN_GILBERT            0
--- test/test/packed/algorithm.cpp
+++ test/test/packed/algorithm.cpp
@@ -1,6 +1,6 @@
 #include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_CHECK, BOOST_CHECK_EQUAL, BOOST_AUTO_TEST_SUITE_END
 #include <boost/test/test_case_template.hpp>    // BOOST_AUTO_TEST_CASE_TEMPLATE
-#include <dctl/packed/packed.hpp>
+#include <dctl/packed/packed.hpp>               // set
 
 namespace dctl {
 namespace packed {
