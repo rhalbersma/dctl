739cff9152ac63fd48bb2f66b8c3ed1b1f4c35f0
rhalbersma <rhalbersma@gmail.com>
2013-05-20 19:14:15 +0200

    clean up tests (fixes issue #27)

--- test/test/bit/lookup.cpp
+++ test/test/bit/lookup.cpp
@@ -4,7 +4,7 @@
 #include <boost/mpl/vector.hpp>                 // vector
 #include <dctl/bit/bit.hpp>
 #include <dctl/bit/lookup.hpp>
-#include <dctl/utility/int.hpp>
+#include <dctl/utility/int.hpp>                 // num_bits
 
 namespace dctl {
 namespace bit {
