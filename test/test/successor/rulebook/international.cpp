cec90a83d67dfd94a49285a512bb97b882317383
Rein Halbersma <Rein Halbersma@localhost>
2013-06-27 10:04:14 +0200

    use template aliases in test::successor

--- test/test/successor/rulebook/international.cpp
+++ test/test/successor/rulebook/international.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_SUITE(SucessorRulebookInternational)
 // Positions from the official International rules (Italian translation):
 // http://www.fid.it/regolamenti/2008/RegTec_CAPO_II.pdf
 
-typedef Fixture<rules::International, board::International> F;
+using F = Fixture<rules::International, board::International>;
 
 BOOST_FIXTURE_TEST_CASE(KingMoveRange, F)
 {
