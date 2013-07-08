632512f3a94f4befbe3272940c4f65113c56fdb7
rhalbersma <rhalbersma@gmail.com>
2013-07-08 08:36:16 +0200

    use template alias in stack allocator

--- include/dctl/successor/copy.hpp
+++ include/dctl/successor/copy.hpp
@@ -17,10 +17,10 @@ void copy(Position const& p, Sequence& moves)
 }
 
 template<bool Color, class Pieces, class Select, class Position>
-stack_vector<Move>::type copy(Position const& p, Arena<Move>::type& mar)
+stack_vector<Move> copy(Position const& p, Arena<Move>& mar)
 {
-        Alloc<Move>::type mal(mar);
-        stack_vector<Move>::type moves(mal);
+        Alloc<Move> mal(mar);
+        stack_vector<Move> moves(mal);
         moves.reserve(DCTL_PP_STACK_RESERVE);
 
         copy<Color, Pieces, Select>(p, moves);
@@ -30,7 +30,7 @@ stack_vector<Move>::type copy(Position const& p, Arena<Move>::type& mar)
 }
 
 template<class Pieces, class Select, class Position>
-stack_vector<Move>::type copy(Position const& p, Arena<Move>::type& mar)
+stack_vector<Move> copy(Position const& p, Arena<Move>& mar)
 {
         return (p.to_move() == Side::black)?
                 copy<Side::black, Pieces, Select>(p, mar) :
@@ -39,7 +39,7 @@ stack_vector<Move>::type copy(Position const& p, Arena<Move>::type& mar)
 }
 
 template<class Position>
-stack_vector<Move>::type copy(Position const& p, Arena<Move>::type& mar)
+stack_vector<Move> copy(Position const& p, Arena<Move>& mar)
 {
         return copy<pieces::all, select::legal>(p, mar);
 }
