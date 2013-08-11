f40b6e4476979f011f83dc9d0744065d52b8d083
rhalbersma <rhalbersma@gmail.com>
2013-08-11 21:34:17 +0200

    update assert message

--- include/dctl/board/dimensions.hpp
+++ include/dctl/board/dimensions.hpp
@@ -6,8 +6,8 @@ namespace board {
 template<int Height, int Width, bool Parity = false>
 struct Dimensions
 {
-        static_assert(Height > 0, "The height needs to be positive.");
-        static_assert(Width > 0, "The width needs to be positive.");
+        static_assert(Height > 0, "The height shall be positive.");
+        static_assert(Width > 0, "The width shall be positive.");
 
         static constexpr auto height = Height;
         static constexpr auto width = Width;
