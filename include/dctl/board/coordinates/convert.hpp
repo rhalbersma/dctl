31ca4193cabf37a46eb823d0c7118d6bfb83ecd9
rein <rein@Mirach-VirtualBox>
2013-08-14 20:59:02 +0200

    give bit <-> square a non-type template square parameter

---
+++ include/dctl/board/coordinates/convert.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <dctl/board/coordinates.hpp>
+#include <dctl/board/coordinates/transform.hpp>
+#include <dctl/angle/degrees.hpp>
+
+namespace dctl {
+namespace board {
+namespace detail {
+
+template<class FromGrid, class DestGrid, int Angle, int N>
+struct transform
+:
+        Coordinates2Square<
+                Coordinates<
+                        DestGrid,
+                        mpl::lazy::rotate<
+                                Square2Coordinates< Square<FromGrid, N> >,
+                                angle::Degrees< Angle >
+                        >::type::row,
+                        mpl::lazy::rotate<
+                                Square2Coordinates< Square<FromGrid, N> >,
+                                angle::Degrees< Angle >
+                        >::type::col
+                >
+        >
+{};
+
+}       // namespace detail
+
+template<class Board, int N>
+struct square_to_bit
+:
+        detail::transform< typename
+                Board::ExternalGrid, typename
+                Board::InternalGrid,
+                Board::orientation,
+                N
+        >
+{};
+
+template<class Board, int N>
+struct bit_to_square
+:
+        detail::transform< typename
+                Board::InternalGrid, typename
+                Board::ExternalGrid,
+                angle::inverse(Board::orientation),
+                N
+        >
+{};
+
+}       // namespace board
+}       // namespace dctl
+
--- include/dctl/board/mask/init.hpp
+++ include/dctl/board/mask/init.hpp
@@ -7,7 +7,7 @@
 #include <boost/mpl/next_prior.hpp>             // prior
 #include <boost/mpl/placeholders.hpp>           // _1
 #include <dctl/board/mask/init_fwd.hpp>         // init, detail::init, detail::test (primary template and partial specialization declarations)
-#include <dctl/board/mask/predicates.hpp>       // square2bit
+#include <dctl/board/coordinates/convert.hpp>   // square2bit
 #include <dctl/utility/int.hpp>                 // BitBoard
 
 namespace dctl {
@@ -52,7 +52,7 @@ struct test
                 boost::mpl::apply< Predicate, Square >::type,
                 boost::mpl::shift_left<
                         boost::mpl::integral_c<BitBoard, 1>,
-                        boost::mpl::int_<square_to_bit< Board, Square >::type::value>
+                        boost::mpl::int_<square_to_bit< Board, Square::value >::type::value>
                 >,
                 boost::mpl::integral_c<BitBoard, 0>
         >::type
