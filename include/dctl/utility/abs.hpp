59b6af1ad96fece8863184e306f46c85e6c0d374
rein <rein@Mirach-VirtualBox>
2013-08-25 15:25:24 +0200

    refactor abs tests

--- include/dctl/utility/abs.hpp
+++ include/dctl/utility/abs.hpp
@@ -5,18 +5,6 @@ namespace dctl {
 namespace util {
 
 template<class T>
-constexpr auto is_negative(T const& value) noexcept
-{
-        return value < 0;
-}
-
-template<class T>
-constexpr auto is_positive(T const& value) noexcept
-{
-        return 0 < value;
-}
-
-template<class T>
 constexpr auto is_bounded(T const& value, std::pair<T, T> const& range) noexcept
 {
         return range.first <= value && value < range.second;
@@ -25,7 +13,7 @@ constexpr auto is_bounded(T const& value, std::pair<T, T> const& range) noexcept
 template<class T>
 constexpr auto abs(T const& value) noexcept
 {
-        return util::is_negative(value)? -value : value;
+        return (value < 0)? -value : value;
 }
 
 template<class T>
@@ -33,7 +21,7 @@ constexpr auto abs_remainder(T const& a, T const& b) noexcept
 {
         // C++ Standard [expr.mul]/4: (a/b)*b + a%b is equal to a
         // NOTE: this implies that the sign of a%b is equal to the sign of a
-        return a % b + (util::is_negative(a % b)? util::abs(b) : T{0});
+        return a % b + (((a % b) < 0)? util::abs(b) : T{0});
 }
 
 }       // util
