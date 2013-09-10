6a09bb5045c97e142edea2a1d6e3ef40d7a8812b
rein <rein@Mirach-VirtualBox>
2013-09-10 22:03:17 +0200

    define cyclic groups and idempotent test

--- test/test/group/cyclic.cpp
+++ test/test/group/cyclic.cpp
@@ -1,9 +1,11 @@
-#include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE, BOOST_CHECK
-#include <dctl/angle/angle.hpp>         // make_angle
-#include <dctl/angle/degrees.hpp>       // D000, D045, D090, D135, D180, D225, D270, D315
-#include <dctl/group/axioms.hpp>        // is_realized
-#include <dctl/group/action.hpp>        // is_realized
-#include <dctl/group/cyclic.hpp>        // CyclicGroup, make_cyclic_group
+#include <algorithm>                            // all_of
+#include <iterator>                             // begin, end
+#include <boost/iterator/counting_iterator.hpp> // counting_iterator
+#include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE, BOOST_CHECK
+#include <dctl/angle/degrees.hpp>               // D000, D045, D090, D135, D180, D225, D270, D315, D360
+#include <dctl/group/action.hpp>                // is_realized
+#include <dctl/group/axioms.hpp>                // is_realized
+#include <dctl/group/cyclic.hpp>                // CyclicGroup, make_cyclic_group
 
 namespace dctl {
 namespace group {
@@ -33,17 +35,27 @@ CyclicGroup const C_N[] =
         C1, C2, C4, C8
 };
 
-BOOST_AUTO_TEST_CASE(AxiomsIsRealized)
+BOOST_AUTO_TEST_CASE(AllGroupAxiomsIsRealized)
 {
-        for (auto const& g: C_N)
-                BOOST_CHECK(axioms::is_realized(g));
+        BOOST_CHECK(
+                std::all_of(std::begin(C_N), std::end(C_N), [](CyclicGroup const& g){
+                        return axioms::is_realized(g);
+                })
+        );
 }
 
-BOOST_AUTO_TEST_CASE(ActionIsRealizedOnAllAngles)
+BOOST_AUTO_TEST_CASE(AllGroupActionIsRealizedOnAllAngles)
 {
-        for (auto const& g: C_N)
-                for (auto i = 0; i != 360; ++i)
-                        BOOST_CHECK(action::is_realized(make_angle(i), g));
+        auto const alpha = boost::counting_iterator<int>(angle::D000);
+        auto const omega = boost::counting_iterator<int>(angle::D360);
+
+        BOOST_CHECK(
+                std::all_of(std::begin(C_N), std::end(C_N), [=](CyclicGroup const& g){
+                        return std::all_of(alpha, omega, [&](int const& i){
+                                return action::is_realized(i, g);
+                        });
+                })
+        );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
