89d1e14a992870d88546b15205a3994bbbf96ddd
rhalbersma <rhalbersma@gmail.com>
2013-11-04 21:34:57 +0100

    rename Idempotent to Involution

--- include/dctl/group/transform.hpp
+++ include/dctl/group/transform.hpp
@@ -3,12 +3,27 @@
 namespace dctl {
 namespace group {
 
+template<class Function, class Object>
+constexpr Object apply(int n, Function op, Object obj)
+{
+        return n ? op(apply(n - 1, op, obj)) : obj;
+}
+
+struct IsInvolution
+{
+        template<class Function, class Object>
+        constexpr auto operator()(Function op, Object obj) const
+        {
+                return apply(2, op, obj) == apply(0, op, obj);
+        }
+};
+
 struct IsIdempotent
 {
         template<class Function, class Object>
         constexpr auto operator()(Function op, Object obj) const
         {
-                return op(op(obj)) == obj;
+                return apply(2, op, obj) == apply(1, op, obj);
         }
 };
 
