406fa9e250238909d021c383a57d8ff20e1c7ab3
rein <rein@Mirach-VirtualBox>
2013-11-04 21:34:57 +0100

    rename Idempotent to Involution

--- test/test/angle/transform.cpp
+++ test/test/angle/transform.cpp
@@ -4,7 +4,7 @@
 #include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE, BOOST_CHECK
 #include <dctl/angle/degrees.hpp>               // _deg2
 #include <dctl/angle/transform.hpp>             // inverse, rotate, mirror
-#include <dctl/group/transform.hpp>             // IsIdempotent
+#include <dctl/group/transform.hpp>             // IsInvolution
 
 namespace dctl {
 namespace angle {
@@ -16,30 +16,30 @@ using namespace std::placeholders;      // _1
 auto const alpha = boost::counting_iterator<int>{  0};
 auto const omega = boost::counting_iterator<int>{360};
 
-BOOST_AUTO_TEST_CASE(InverseIsIdempotentOnAllAngles)
+BOOST_AUTO_TEST_CASE(InverseIsInvolutionOnAllAngles)
 {
         BOOST_CHECK(
                 std::all_of(alpha, omega, [](auto const& i){
-                        return group::IsIdempotent()(std::bind(angle::inverse2, _1), Angle{i});
+                        return group::IsInvolution()(std::bind(dctl::inverse2, _1), Angle{i});
                 })
         );
 }
 
-BOOST_AUTO_TEST_CASE(Rotate180IsIdempotentOnAllAngles)
+BOOST_AUTO_TEST_CASE(Rotate180IsInvolutionOnAllAngles)
 {
         BOOST_CHECK(
                 std::all_of(alpha, omega, [](auto const& i){
-                        return group::IsIdempotent()(std::bind(angle::rotate2, _1, 180_deg2), Angle{i});
+                        return group::IsInvolution()(std::bind(dctl::rotate2, _1, 180_deg2), Angle{i});
                 })
         );
 }
 
-BOOST_AUTO_TEST_CASE(MirrorIsIdempotentOnAllAnglePairs)
+BOOST_AUTO_TEST_CASE(MirrorIsInvolutionOnAllAnglePairs)
 {
         BOOST_CHECK(
                 std::all_of(alpha, omega, [](auto const& i){
                         return std::all_of(alpha, omega, [=](int const& j) {
-                                return group::IsIdempotent()(std::bind(angle::mirror2, _1, Angle{i}), Angle{j});
+                                return group::IsInvolution()(std::bind(dctl::mirror2, _1, Angle{i}), Angle{j});
                         });
                 })
         );
