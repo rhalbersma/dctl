0943ea765cd1a863cb84fc7e59e9f6c4a6a4e32a
rhalbersma <rhalbersma@gmail.com>
2013-11-04 14:06:51 +0100

    remove dependency on handwritten Boost.MPL extensions

--- include/dctl/preprocessor/mpl/lazy_binary_metafunction_decl.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define DCTL_PP_LAZY_BINARY_METAFUNCTION_DECL(F)        \
-namespace lazy {                                        \
-template<class X1, class X2>                            \
-struct F;                                               \
-}       // namespace lazy
--- include/dctl/preprocessor/mpl/lazy_unary_metafunction_decl.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define DCTL_PP_LAZY_UNARY_METAFUNCTION_DECL(F)         \
-namespace lazy {                                        \
-template<class X>                                       \
-struct F;                                               \
-}       // namespace lazy
