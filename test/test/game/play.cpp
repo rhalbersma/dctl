12ae495ecbcc1a274e58051f354de6b708436825
Rein Halbersma <Rein Halbersma@localhost>
2013-12-27 15:11:28 +0100

    refactor game play program
    - input validation and default choice upon hitting Enter

--- test/test/game/play.cpp
+++ test/test/game/play.cpp
@@ -2,21 +2,23 @@
 #include <iomanip>
 #include <iostream>
 #include <stack>
-#include <dctl/board/types.hpp>
 #include <dctl/node/position.hpp>
 #include <dctl/move/ostream.hpp>
-#include <dctl/rules/variants.hpp>
 #include <dctl/setup/diagram.hpp>
 #include <dctl/setup/string.hpp>
 #include <dctl/successor/generate.hpp>
 #include <dctl/utility/stack_vector.hpp>
+
+// customize rules and board form these headers
+#include <dctl/rules/variants.hpp>
+#include <dctl/board/types.hpp>
 
 using namespace dctl;
 
 int main()
 {
         using Rules = rules::International;
-        using Board = board::Mini;
+        using Board = board::International;
         using Pos = Position<Rules, Board>;
 
         auto initial = Pos::initial();
@@ -29,22 +31,52 @@ int main()
 
                 auto moves = successor::generate(p);
                 std::sort(begin(moves), end(moves), [](auto const& L, auto const& R) {
-                        return L.algebraic() < R.algebraic();
+                        return format::as_algebraic(L) < format::as_algebraic(R);
                 });
 
                 if (moves.empty()) {
-                        std::cout << "\nNo valid moves (-1 to stop / -2 to undo): ";
+                        std::cout << "\nNo valid moves (Undo or stop [U/s]): ";
                 } else {
                         auto index = 0;
                         for (auto const& m : moves)
                                 std::cout << std::setw(2) << index++ << "." << m << '\n';
-                        std::cout << "\nEnter move number (-1 to stop / -2 to undo): ";
+                        std::cout << "\nEnter move number [0], undo or stop [N/u/s]): ";
                 }
 
-                int choice;
-                std::cin >> choice;
+                int choice = 0;
 
-                if (choice == -1) break;
+                for (std::string input; std::getline(std::cin, input);) {
+                        std::transform(begin(input), end(input), begin(input), ::tolower);
+
+                        if (moves.empty()) {
+                                if (input.empty() || input == "u")
+                                        choice = -2;
+                                else if (input == "s")
+                                        choice = -1;
+                                else {
+                                        std::cout << "Invalid input\n";
+                                        continue;
+                                }
+                        } else {
+                                std::stringstream sstr(input);
+                                if (!(sstr >> choice)) {
+                                        if (input.empty())
+                                                choice = 0;
+                                        else if (input == "s")
+                                                choice = -1;
+                                        else if (input == "u")
+                                                choice = -2;
+                                        else {
+                                                std::cout << "Invalid input\n";
+                                                continue;
+                                        }
+                                }
+                        }
+                        break;
+                }
+
+                if (choice == -1)
+                        break;
                 if (choice == -2) {
                         if (game.size() > 1)
                                 game.pop();
