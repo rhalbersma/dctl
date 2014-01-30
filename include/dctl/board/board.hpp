0ffc956593cec54040d36c3369aba4b2361bf5cc
rhalbersma <rhalbersma@gmail.com>
2014-01-30 22:20:19 +0100

    fix bug in minimal board size computation

--- include/dctl/board/board.hpp
+++ include/dctl/board/board.hpp
@@ -35,7 +35,7 @@ public:
 
 private:
         using Block = uint64_t;
-        static constexpr auto Nb = internal_grid::size / std::numeric_limits<Block>::digits + 1;
+        static constexpr auto Nb = (internal_grid::size - 1) / std::numeric_limits<Block>::digits + 1;
         static constexpr auto N = Nb * std::numeric_limits<Block>::digits;
 
 public:
