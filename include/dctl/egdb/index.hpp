2229cbf1064db5f7fef5f1b478d720e4bb4ec4f5
Rein Halbersma <Rein Halbersma@localhost>
2014-02-20 22:56:36 +0100

    implement colex rank/unrank for arbitrary combinations
    exhaustively tested that rank/unrank are inversely related on Choose(50,8)
    speed at around 2 million positions per second

--- include/dctl/egdb/index.hpp
+++ include/dctl/egdb/index.hpp
@@ -1,68 +1,45 @@
 #pragma once
 #include <algorithm>                            // accumulate
 #include <cassert>                              // assert
+#include <iterator>
+#include <utility>
 #include <boost/range/adaptors.hpp>
 #include <boost/range/algorithm.hpp>
+#include <boost/range/irange.hpp>
 #include <boost/range/numeric.hpp>
 #include <dctl/bit/detail/intrinsic.hpp>        // count
 #include <dctl/egdb/binomial.hpp>
+#include <dctl/bit/set.hpp>
 
 namespace dctl {
 namespace egdb {
 
-template<class SinglePassRange>
-auto colex_subset_rank(SinglePassRange const& rng)
+template<class SinglePassRange, class Generator = Binomial<384, 192>>
+auto colex_combination_rank(SinglePassRange const& rng, Generator const& = Generator{})
 {
         return boost::accumulate(
-                rng, 0, [i = 1](auto index, auto sq) mutable {
-                return index + Binomial<384, 10>::coefficient(sq, i++);
+                rng, typename Generator::value_type{0}, [i = 1](auto index, auto sq) mutable {
+                return index + Generator::coefficient(sq, i++);
         });
 }
 
-/*
-template<class Set>
-auto remove_interference(Set const& s, Set const& interference)
+template<class T, class Generator = Binomial<384, 192>>
+auto colex_combination_unrank(T rank, std::pair<int, int> const& combination, Generator const& = Generator{})
 {
-        return s | boost::adaptors::transformed([](auto const& sq){
-                return bit::popcount(interference.before(sq));
+        auto N = combination.first;
+        auto K = combination.second;
+        bit::Set<int, uint64_t, 1> rng;
+        boost::transform(
+                boost::irange(1, K + 1) | boost::adaptors::reversed,
+                std::inserter(rng, begin(rng)), [&](auto k) {
+                N = *find_if(boost::irange(0, N) | boost::adaptors::reversed, [=](auto n) {
+                        return Generator::coefficient(n, k) <= rank;
+                });
+                rank -= Generator::coefficient(N, k);
+                return N;
         });
+        return rng;
 }
 
-template<class Set, class Index>
-auto placement_from_index(Index index, Set const& interference)
-{
-        return colex_subset_rank(remove_interference(set, pattern));
-}
-
-template<class Position>
-auto index_from_position(Position const& p)
-{
-        constexpr auto board_size = Position::board_type::size();
-        auto const num_bk = p.kings(Side::black).count();
-        auto const num_wk = p.kings(Side::white).count();
-        auto const bk_range = binomial(board_size, num_bk);
-        auto const wk_range = binomial(board_size - num_bk, num_wk);
-
-        auto constexpr ghosts = ~board::Squares<typename Position::board_type>::mask();
-        auto const bk_index = index_from_placement(p.kings(Side::black), ghosts);
-        auto const wk_index = index_from_placement(p.kings(Side::white), ghosts ^ p.kings(Side::black));
-        assert(0 <= bk_index && bk_index < bk_range);
-        assert(0 <= wk_index && wk_index < wk_range);
-
-        return bk_index + wk_index * bk_range;
-}
-
-template<class Position, class Index>
-auto position_from_index(Index index)
-{
-        constexpr auto board_size = Position::board_type::size();
-        auto const bk_range = binomial(board_size, Index::num_bk);
-        auto const wk_range = binomial(board_size - Index::num_bk, Index::num_wk);
-
-        auto const bk_index = index % bk_range;
-        auto const wk_index = index / bk_range;
-
-}
-*/
 }       // namespace egdb
 }       // namespace dctl
