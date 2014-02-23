e92829feeb1bbe7b57c9baa9cdd62e85849f043e
Rein Halbersma <Rein Halbersma@localhost>
2014-02-23 22:40:13 +0100

    add C-style indexing/deindexing for comparison

--- test/test/egdb/index.cpp
+++ test/test/egdb/index.cpp
@@ -11,6 +11,8 @@
 #include <iostream>
 #include <iomanip>
 #include <boost/range/irange.hpp>
+#include <dctl/board/types.hpp>
+#include <dctl/board/mask.hpp>
 
 namespace dctl {
 namespace egdb {
@@ -37,14 +39,54 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(ColexSubsetRank, T, SetTypes)
 BOOST_AUTO_TEST_CASE_TEMPLATE(ColexSubsetUnRank, T, SetTypes)
 {
         using binomial = Binomial<384, 192>;
-        auto N = 50; auto K = 8;
+        auto N = 50; auto K = 2;
 
         auto b = std::ptrdiff_t{0};
         auto e = binomial::coefficient(N, K);
 
+        using Board = board::International;
+        auto pattern = ~board::Squares<Board>::mask();
+        for (auto sq : pattern)
+                std::cout << sq << ",";
+        std::cout << "\n";
+
+        auto x = boost::irange(0, 50) | boost::adaptors::transformed([&](auto i) {
+                return i + bit::intrinsic::popcount(pattern.data() & ((1ULL << i) - 1));
+        });
+        for (auto j : x)
+                std::cout << j << ",";
+
         for (auto i : boost::irange(b, e)) {
-                auto pos = colex_combination_unrank(i, {N,K});
-                auto idx = colex_combination_rank(pos);
+                auto pos = colex_combination_unrank<T>(i, {N,K});
+                auto ipos = mixin(pos, pattern);
+                auto opos = mixout(ipos, pattern);
+                auto idx = colex_combination_rank(opos);
+                std::cout << "   i =" <<   i << ", ";
+                std::cout << " pos ="; std::copy(begin(pos), end(pos), std::ostream_iterator<int>(std::cout, ","));
+                std::cout << " ipos="; boost::copy(ipos, std::ostream_iterator<int>(std::cout, ","));
+                std::cout << " opos="; boost::copy(opos, std::ostream_iterator<int>(std::cout, ","));
+                std::cout << " idx =" << idx << "\n";
+
+                //BOOST_CHECK_EQUAL(idx, i);
+        }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(ColexSubsetUnRank2, T, SetTypes)
+{
+        using binomial = Binomial<384, 192>;
+        auto N = 50; auto K = 1;
+
+        auto b = std::ptrdiff_t{0};
+        auto e = binomial::coefficient(N, K);
+
+        using R = bit::InputRange<int, uint64_t, 1>;
+        using Board = board::International;
+        auto const pattern = T{0};//board::Squares<Board>::mask();
+
+        for (auto i : boost::irange(b, e)) {
+                auto t = colex_combination_unrank2<T>(i, {N,K});
+                auto pos = mixin2<T>(R{t.data()}, pattern);
+                auto idx = colex_combination_rank2_mixout(R{pos.data()}, pattern);
                 BOOST_CHECK_EQUAL(idx, i);
         }
 }
