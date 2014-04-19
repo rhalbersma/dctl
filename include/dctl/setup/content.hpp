c890a56ab7f8247a1a5d60d5e67b92374e3b1edf
Rein Halbersma <Rein Halbersma@localhost>
2014-04-20 00:09:03 +0200

    formatting comments

--- include/dctl/setup/content.hpp
+++ include/dctl/setup/content.hpp
@@ -11,14 +11,14 @@ std::string content(Position const& p, int i)
         std::stringstream sstr;
         if (p.pieces(Color::black).test(i)) {
                 if (p.kings().test(i))
-                        sstr << Token::upper[Color::black];      // black king
+                        sstr << Token::upper[Color::black];     // black king
                 else
-                        sstr << Token::lower[Color::black];      // black man
+                        sstr << Token::lower[Color::black];     // black pawn
         } else if (p.pieces(Color::white).test(i)) {
                 if (p.kings().test(i))
-                        sstr << Token::upper[Color::white];      // white king
+                        sstr << Token::upper[Color::white];     // white king
                 else
-                        sstr << Token::lower[Color::white];      // white man
+                        sstr << Token::lower[Color::white];     // white pawn
         } else
                 sstr << Token::empty;                           // empty square
         return sstr.str();
