a836c7506c63e55a16221510fbfff524d2757c02
rhalbersma <rhalbersma@gmail.com>
2014-08-12 20:50:44 +0200

    use operator notation in unit tests for bit algorithms

--- test/src/bit/algorithm.cpp
+++ test/src/bit/algorithm.cpp
@@ -84,12 +84,12 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(SetIntersection, T, SetTypes)
         constexpr auto a = T { 0, 1, 2, T::max_size() - 2, T::max_size() - 1 };
         constexpr auto b = T { 0, 1, 3, T::max_size() - 4, T::max_size() - 3 };
 
-        constexpr auto i_ab = set_intersection(a, b);
-        constexpr auto i_ba = set_intersection(b, a);
+        constexpr auto i_ab = a & b;
+        constexpr auto i_ba = b & a;
         BOOST_CHECK_EQUAL_COLLECTIONS(begin(i_ab), end(i_ab), begin(i_ba), end(i_ba));
 
-        BOOST_CHECK(includes(a, i_ab));
-        BOOST_CHECK(includes(b, i_ab));
+        BOOST_CHECK(i_ab.is_subset_of(a));
+        BOOST_CHECK(i_ab.is_subset_of(b));
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(SetUnion, T, SetTypes)
@@ -97,12 +97,12 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(SetUnion, T, SetTypes)
         constexpr auto a = T { 0, 1, 2, T::max_size() - 2, T::max_size() - 1 };
         constexpr auto b = T { 0, 1, 3, T::max_size() - 4, T::max_size() - 3 };
 
-        constexpr auto u_ab = set_union(a, b);
-        constexpr auto u_ba = set_union(b, a);
+        constexpr auto u_ab = a | b;
+        constexpr auto u_ba = b | a;
         BOOST_CHECK_EQUAL_COLLECTIONS(begin(u_ab), end(u_ab), begin(u_ba), end(u_ba));
 
-        BOOST_CHECK(includes(u_ab, a));
-        BOOST_CHECK(includes(u_ab, b));
+        BOOST_CHECK(a.is_subset_of(u_ab));
+        BOOST_CHECK(b.is_subset_of(u_ab));
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(SetSymmetricDifference, T, SetTypes)
@@ -110,14 +110,14 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(SetSymmetricDifference, T, SetTypes)
         constexpr auto a = T { 0, 1, 2, T::max_size() - 2, T::max_size() - 1 };
         constexpr auto b = T { 0, 1, 3, T::max_size() - 4, T::max_size() - 3 };
 
-        constexpr auto s_ab = set_symmetric_difference(a, b);
-        constexpr auto s_ba = set_symmetric_difference(b, a);
+        constexpr auto s_ab = a ^ b;
+        constexpr auto s_ba = b ^ a;
         BOOST_CHECK_EQUAL_COLLECTIONS(begin(s_ab), end(s_ab), begin(s_ba), end(s_ba));
 
-        constexpr auto u = set_union(set_difference(a, b), set_difference(b, a));
+        constexpr auto u = (a - b) | (b - a);
         BOOST_CHECK_EQUAL_COLLECTIONS(begin(s_ab), end(s_ab), begin(u), end(u));
 
-        constexpr auto d = set_difference(set_union(a, b), set_intersection(a, b));
+        constexpr auto d = (a | b) - (a & b);
         BOOST_CHECK_EQUAL_COLLECTIONS(begin(s_ab), end(s_ab), begin(d), end(d));
 }
 
@@ -126,13 +126,13 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(SetDifference, T, SetTypes)
         constexpr auto a = T { 0, 1, 2, T::max_size() - 2, T::max_size() - 1 };
         constexpr auto b = T { 0, 1, 3, T::max_size() - 4, T::max_size() - 3 };
 
-        constexpr auto d_ab = set_difference(a, b);
-        constexpr auto d_ba = set_difference(b, a);
+        constexpr auto d_ab = a - b;
+        constexpr auto d_ba = b - a;
 
-        BOOST_CHECK( includes(a, d_ab));
-        BOOST_CHECK(!includes(a, d_ba));
-        BOOST_CHECK( includes(b, d_ba));
-        BOOST_CHECK(!includes(b, d_ab));
+        BOOST_CHECK( d_ab.is_subset_of(a));
+        BOOST_CHECK(!d_ba.is_subset_of(a));
+        BOOST_CHECK( d_ba.is_subset_of(b));
+        BOOST_CHECK(!d_ab.is_subset_of(b));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
