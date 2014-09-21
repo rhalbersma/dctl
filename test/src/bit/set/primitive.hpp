bc21f96050c1d0e0161bd019a6e31f18a342b110
Rein Halbersma <Rein Halbersma@localhost>
2014-09-21 21:54:50 +0200

    more exhaustive testing for bitset

--- test/src/bit/set/primitive.hpp
+++ test/src/bit/set/primitive.hpp
@@ -4,22 +4,106 @@ namespace dctl {
 namespace bit {
 
 template<class T, int N = T::size()>
+constexpr auto and_assign(T const& lhs, T const& rhs) noexcept
+{
+        // arrange
+        auto next = lhs;
+        auto const prev = lhs;
+
+        // act
+        next &= rhs;
+
+        // assert
+        auto check = true;
+        for (auto i = 0; i < N; ++i) {
+                if (!rhs.test(i))
+                        check &= !next.test(i);
+                else
+                        check &= next.test(i) == prev.test(i);
+        }
+        return check;
+}
+
+template<class T, int N = T::size()>
+constexpr auto or_assign(T const& lhs, T const& rhs) noexcept
+{
+        // arrange
+        auto next = lhs;
+        auto const prev = lhs;
+
+        // act
+        next |= rhs;
+
+        // assert
+        auto check = true;
+        for (auto i = 0; i < N; ++i) {
+                if (rhs.test(i))
+                        check &= next.test(i);
+                else
+                        check &= next.test(i) == prev.test(i);
+        }
+        return check;
+}
+
+template<class T, int N = T::size()>
+constexpr auto xor_assign(T const& lhs, T const& rhs) noexcept
+{
+        // arrange
+        auto next = lhs;
+        auto const prev = lhs;
+
+        // act
+        next ^= rhs;
+
+        // assert
+        auto check = true;
+        for (auto i = 0; i < N; ++i) {
+                if (rhs.test(i))
+                        check &= next.test(i) == !prev.test(i);
+                else
+                        check &= next.test(i) == prev.test(i);
+        }
+        return check;
+}
+
+template<class T, int N = T::size()>
+constexpr auto minus_assign(T const& lhs, T const& rhs) noexcept
+{
+        // arrange
+        auto next = lhs;
+        auto const prev = lhs;
+
+        // act
+        next -= rhs;
+
+        // assert
+        auto check = true;
+        for (auto i = 0; i < N; ++i) {
+                if (rhs.test(i))
+                        check &= !next.test(i);
+                else
+                        check &= next.test(i) == prev.test(i);
+        }
+        return check;
+}
+
+template<class T, int N = T::size()>
 constexpr auto shift_left_assign(T const& b, int n)
 {
         // arrange
-        auto rhs = b;
-        auto lhs = rhs;
+        auto next = b;
+        auto const prev = b;
 
         // act
-        lhs <<= n;
+        next <<= n;
 
         // assert
         auto check = true;
         for (auto i = 0; i < N; ++i) {
                 if (i < n)
-                        check &= lhs.test(i) == 0;
+                        check &= !next.test(i);
                 else
-                        check &= lhs.test(i) == rhs.test(i - n);
+                        check &= next.test(i) == prev.test(i - n);
         }
         return check;
 }
@@ -28,49 +112,113 @@ template<class T, int N = T::size()>
 constexpr auto shift_right_assign(T const& b, int n)
 {
         // arrange
-        auto rhs = b;
-        auto lhs = rhs;
+        auto next = b;
+        auto const prev = b;
 
         // act
-        lhs >>= n;
+        next >>= n;
 
         // assert
         auto check = true;
         for (auto i = 0; i < N; ++i) {
                 if (i > N - 1 - n)
-                        check &= lhs.test(i) == 0;
+                        check &= !next.test(i);
                 else
-                        check &= lhs.test(i) == rhs.test(i + n);
+                        check &= next.test(i) == prev.test(i + n);
         }
         return check;
+}
+
+template<class T, int N = T::size()>
+constexpr auto count(T const& b) noexcept
+{
+        // arrange
+        auto result = 0;
+
+        // act
+        for (auto i = 0; i < N; ++i)
+                result += b.test(i);
+
+        // assert
+        return b.count() == result;
+}
+
+template<class T>
+constexpr auto op_and(T const& lhs, T const& rhs)
+{
+        // arrange
+        auto next = lhs;
+
+        // act
+        next &= rhs;
+
+        // assert
+        return (lhs & rhs) == next;
+}
+
+template<class T>
+constexpr auto op_or(T const& lhs, T const& rhs)
+{
+        // arrange
+        auto next = lhs;
+
+        // act
+        next |= rhs;
+
+        // assert
+        return (lhs | rhs) == next;
+}
+
+template<class T>
+constexpr auto op_xor(T const& lhs, T const& rhs)
+{
+        // arrange
+        auto next = lhs;
+
+        // act
+        next ^= rhs;
+
+        // assert
+        return (lhs ^ rhs) == next;
+}
+
+template<class T>
+constexpr auto op_minus(T const& lhs, T const& rhs)
+{
+        // arrange
+        auto next = lhs;
+
+        // act
+        next -= rhs;
+
+        // assert
+        return (lhs - rhs) == next;
 }
 
 template<class T>
 constexpr auto shift_left(T const& b, int n)
 {
         // arrange
-        auto rhs = b;
-        auto lhs = rhs << n;
+        auto next = b;
 
         // act
-        rhs <<= n;
+        next <<= n;
 
         // assert
-        return lhs == rhs;
+        return (b << n) == next;
 }
 
 template<class T>
 constexpr auto shift_right(T const& b, int n)
 {
         // arrange
-        auto rhs = b;
-        auto lhs = rhs >> n;
+        auto next = b;
 
         // act
-        rhs >>= n;
+        next >>= n;
 
         // assert
-        return lhs == rhs;
+        return (b >> n) == next;
 }
 
 }       // namespace bit
