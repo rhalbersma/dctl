6e795adfa7d1287a3ae80beb9fec96e9e319ee75
rhalbersma <rhalbersma@gmail.com>
2014-11-08 23:27:20 +0100

    eliminate Boost.MPL for compile-time computations

--- include/dctl/grid/grid_fwd.hpp
+++ include/dctl/grid/grid_fwd.hpp
@@ -3,6 +3,12 @@
 namespace dctl {
 namespace grid {
 
+template<int EdgeColumns>
+class Grid;
+
+template<>
+class Grid<0>;
+
 // primary template declaration
 template<class Dimensions, int EdgeColumns>
 class GridClass;
