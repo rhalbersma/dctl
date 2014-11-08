fcc5b0cff7103cb3cf4938f212fc190f7f9abc02
Rein Halbersma <Rein Halbersma@localhost>
2014-11-08 23:27:49 +0100

    clean up Angle

--- test/src/angle/traits.cpp
+++ test/src/angle/traits.cpp
@@ -47,7 +47,7 @@ BOOST_AUTO_TEST_CASE(LeftIsNotRight)
 
 BOOST_AUTO_TEST_CASE(RightIsNotLeft)
 {
-        for (auto a : { 315_deg, 0_deg, 45_deg }) {
+        for (auto&& a : { 315_deg, 0_deg, 45_deg }) {
                 BOOST_CHECK( is_right(a));
                 BOOST_CHECK(!is_left (a));
         }
@@ -63,7 +63,7 @@ BOOST_AUTO_TEST_CASE(PositiveIsNotNegative)
 
 BOOST_AUTO_TEST_CASE(NegativeIsNotPositive)
 {
-        for (auto a : { 225_deg, 270_deg, 315_deg, 0_deg }) {
+        for (auto&& a : { 225_deg, 270_deg, 315_deg, 0_deg }) {
                 BOOST_CHECK( is_negative(a));
                 BOOST_CHECK(!is_positive(a));
         }
