41ecdc1055592b86b2ec91436998f02891c105ca
Rein Halbersma <Rein Halbersma@localhost>
2014-11-13 22:11:18 +0100

    grid dimensions is now a plain struct instead of a class

--- include/dctl/grid/grid.hpp
+++ include/dctl/grid/grid.hpp
@@ -14,17 +14,17 @@ class Grid<0>
 private:
         // range of even (e) and odd (o) rows
 
-        constexpr auto row_e() const { return (width() +  ul_parity()) / 2; }
-        constexpr auto row_o() const { return (width() + !ul_parity()) / 2; }
+        constexpr auto row_e() const { return (width +  ul_parity(*this)) / 2; }
+        constexpr auto row_o() const { return (width + !ul_parity(*this)) / 2; }
 
 public:
-        constexpr Grid(Dimensions const& dim) : Dimensions{dim} {}
+        constexpr Grid(Dimensions const& dim) : Dimensions(dim) {}
 
         constexpr auto edge_columns() const { return 0; }
 
         // range of row pairs
 
-        constexpr auto modulo() const { return width(); }
+        constexpr auto modulo() const { return width; }
 
         // left (l) and right (r) edges of even (e) and odd (o) rows
 
@@ -35,7 +35,7 @@ public:
 
         // grid size
 
-        constexpr int size() const { return (width() * height()) / 2 + (width() * height() * ul_parity()) % 2; }
+        constexpr int size() const { return (width * height) / 2 + (width * height * ul_parity(*this)) % 2; }
 };
 
 template<int EdgeColumns>
@@ -47,13 +47,13 @@ private:
         Grid<0> BaseGrid;
 
 public:
-        constexpr Grid(Dimensions const& dim) : Dimensions{dim}, BaseGrid{dim} {}
+        constexpr Grid(Dimensions const& dim) : Dimensions(dim), BaseGrid{dim} {}
 
         constexpr auto edge_columns() const { return EdgeColumns; }
 
         // range of row pairs
 
-        constexpr auto left_down() const { return (width() + EdgeColumns) / 2; }
+        constexpr auto left_down() const { return (width + EdgeColumns) / 2; }
         constexpr auto right_down() const { return left_down() + 1; }
         constexpr auto modulo() const { return left_down() + right_down(); }
 
@@ -61,12 +61,12 @@ public:
 
         constexpr auto edge_le() const { return BaseGrid.edge_le(); }
         constexpr auto edge_re() const { return BaseGrid.edge_re(); }
-        constexpr auto edge_lo() const { return left_down() + ul_parity(); }
+        constexpr auto edge_lo() const { return left_down() + ul_parity(*this); }
         constexpr auto edge_ro() const { return edge_lo() + (BaseGrid.edge_ro() - BaseGrid.edge_lo()); }
 
         // grid size
 
-        constexpr auto size() const { return modulo() * ((height() - 1) / 2) + ((height() % 2) ? edge_re() : edge_ro()) + 1; }
+        constexpr auto size() const { return modulo() * ((height - 1) / 2) + ((height % 2) ? edge_re() : edge_ro()) + 1; }
 };
 
 
