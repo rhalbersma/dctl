69338eeb50dd4d35ee9c85bd91276a286b9c7bb1
Rein Halbersma <Rein Halbersma@localhost>
2014-11-16 23:06:58 +0100

    define align_on function

--- include/dctl/set_type.hpp
+++ include/dctl/set_type.hpp
@@ -1,12 +1,17 @@
 #pragma once
 #include <xstd/bitset.hpp>      // bitset
+#include <cassert>              // assert
 
 namespace dctl {
 
-template<class Board>
-constexpr auto set_size = Board::bits();
+template<class T>
+constexpr auto align_on(T offset, T align) noexcept
+{
+        assert(align && !(align & (align - 1)));
+        return (offset + align - 1) & ~(align - 1);
+}
 
 template<class Board>
-using set_type = xstd::bitset<set_size<Board>>;
+using set_type = xstd::bitset<align_on(Board::bits(), 64)>;
 
 }       // dctl
