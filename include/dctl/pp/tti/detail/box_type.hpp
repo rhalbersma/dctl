2656fe7ce9bb7e794e651b5e29767d6845eff877
Rein Halbersma <Rein Halbersma@localhost>
2014-12-29 00:18:32 +0100

    move preprocessor macros to <xstd/pp/tti.hpp>

--- include/dctl/pp/tti/detail/box_type.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DCTL_PP_TTI_BOX_TYPE(NAME)      \
-                                        \
-template<class T>                       \
-struct box_type_ ## NAME                \
-{                                       \
-        using type = typename T::NAME;  \
-};                                      \
