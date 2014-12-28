e276ffc33a61725a7cdf6755dba5f590c03be12e
rhalbersma <rhalbersma@gmail.com>
2014-12-29 00:18:32 +0100

    move preprocessor macros to <xstd/pp/tti.hpp>

--- include/dctl/pp/tti.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <dctl/pp/tti/static_constant_no_default.hpp>   // DCTL_PP_TTI_STATIC_CONSTANT_NO_DEFAULT
-#include <dctl/pp/tti/static_constant.hpp>              // DCTL_PP_TTI_STATIC_CONSTANT
-#include <dctl/pp/tti/type.hpp>                         // DCTL_PP_TTI_TYPE
