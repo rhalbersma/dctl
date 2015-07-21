f7dad2eef93424beea76f0ce96ce4db2a2f0fac0
Rein Halbersma <rhalbersma@gmail.com>
2015-07-21 22:02:30 +0200

    adding multiple implementations of piece placement

---
+++ include/dctl/state/piece_placement/cp22e/piece_placement.hpp
@@ -0,0 +1,92 @@
+#pragma once
+#include <dctl/color.hpp>
+#include <dctl/piece.hpp>
+#include <dctl/set_type.hpp>
+#include <dctl/state/piece_placement/invariant.hpp>
+#include <xstd/type_traits.hpp>                 // to_underlying_type
+#include <cassert>                              // assert
+
+namespace dctl {
+namespace detail {
+namespace cp22e {
+
+template<class Board>
+class PiecePlacement
+{
+public:
+        using board_type = Board;
+        using set_type = get_set_type<Board>;
+
+        set_type by_color_piece[2][2];
+        set_type not_occupied_;
+
+public:
+        PiecePlacement() = default;
+
+        PiecePlacement(set_type const& b, set_type const& w, set_type const& p, set_type const& k)
+        :
+                by_color_piece{{b & p, b & k}, {w & p, w & k}},
+                not_occupied_{board::squares_v<Board> ^ (b | w)}
+        {
+                assert(invariant(*this));
+        }
+
+        template<class Action>
+        auto& make(Action const& a)
+        {
+                pieces(a.to_move(), a.with()).reset(a.from());
+                pieces(a.to_move(), a.into()).set  (a.dest());
+
+                if (a.is_jump()) {
+                        pieces(!a.to_move(), Piece::pawn) &= ~a.captured();
+                        pieces(!a.to_move(), Piece::king) &= ~a.captured();
+                        not_occupied_ ^= a.captured();
+                }
+
+                not_occupied_.set  (a.from());
+                not_occupied_.reset(a.dest());
+
+                assert(invariant(*this));
+                return *this;
+        }
+
+        auto pieces(Color c) const noexcept
+        {
+                return pieces(c, Piece::pawn) ^ pieces(c, Piece::king);
+        }
+
+        auto pieces(Piece p) const noexcept
+        {
+                return pieces(Color::black, p) ^ pieces(Color::white, p);
+        }
+
+        auto pieces(Color c, Piece p) const noexcept
+        {
+                return by_color_piece[xstd::to_underlying_type(c)][xstd::to_underlying_type(p)];
+        }
+
+        auto pieces() const noexcept
+        {
+                return board::squares_v<Board> ^ not_occupied();
+        }
+
+        auto not_occupied() const noexcept
+        {
+                return not_occupied_;
+        }
+
+        auto num_pieces(Color c, Piece p) const noexcept
+        {
+                return pieces(c, p).count();
+        }
+
+private:
+        auto& pieces(Color c, Piece p) noexcept
+        {
+                return by_color_piece[xstd::to_underlying_type(c)][xstd::to_underlying_type(p)];
+        }
+};
+
+}       // namespace cp22e
+}       // namespace detail
+}       // namespace dctl
---
+++ include/dctl/state/piece_placement/invariant.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <dctl/board/mask.hpp>  // squares, Promotion
+#include <dctl/color.hpp>       // black, white
+#include <dctl/piece.hpp>       // pawn, king
+#include <dctl/type_traits.hpp> // board_type
+
+namespace dctl {
+namespace detail {
+
+template<class PiecePlacement>
+constexpr auto invariant(PiecePlacement const& p) noexcept
+{
+        using board_type = board_type_t<PiecePlacement>;
+
+        return
+                 (p.pieces(Color::black, Piece::pawn) ^ p.pieces(Color::black, Piece::king)) == p.pieces(Color::black) &&
+                 (p.pieces(Color::white, Piece::pawn) ^ p.pieces(Color::white, Piece::king)) == p.pieces(Color::white) &&
+                 (p.pieces(Color::black, Piece::pawn) ^ p.pieces(Color::white, Piece::pawn)) == p.pieces(Piece::pawn ) &&
+                 (p.pieces(Color::black, Piece::king) ^ p.pieces(Color::white, Piece::king)) == p.pieces(Piece::king ) &&
+                 (p.pieces(Color::black) ^ p.pieces(Color::white)) == p.pieces() &&
+                 (p.pieces(Piece::pawn ) ^ p.pieces(Piece::king )) == p.pieces() &&
+                 (p.pieces() ^ p.not_occupied()) == board::squares_v<board_type> &&
+                 disjoint(p.pieces(Color::black), p.pieces(Color::white)) &&
+                 disjoint(p.pieces(Piece::pawn ), p.pieces(Piece::king )) &&
+                 disjoint(p.pieces(), p.not_occupied()) &&
+                 disjoint(p.pieces(Color::black, Piece::pawn), board::Promotion<board_type>::mask(Color::black)) &&
+                 disjoint(p.pieces(Color::white, Piece::pawn), board::Promotion<board_type>::mask(Color::white))
+        ;
+}
+
+}       // namespace detail
+}       // namespace dctl
