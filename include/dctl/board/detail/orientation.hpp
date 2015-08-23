32adf42d5d817eb3d3e79220bc11f8539bc08e17
rhalbersma <rhalbersma@gmail.com>
2015-08-23 22:30:32 +0200

    use C++1z to get constexpr min_element

--- include/dctl/board/detail/orientation.hpp
+++ include/dctl/board/detail/orientation.hpp
@@ -1,8 +1,8 @@
 #pragma once
-#include <dctl/board/angle.hpp>       // Angle, _deg
+#include <dctl/board/angle.hpp> // Angle, _deg
 #include <dctl/board/grid.hpp>  // Grid, rotate
-#include <xstd/algorithm.hpp>   // min_element
-#include <xstd/iterator.hpp>    // cbegin, cend
+#include <algorithm>            // min_element
+#include <iterator>             // cbegin, cend
 
 namespace dctl {
 namespace board {
@@ -21,7 +21,7 @@ constexpr auto size_minimizing_orientation(OuterGrid const& g)
         };
 
         constexpr Angle orientations[] = { 0_deg, 90_deg, 180_deg, 270_deg };
-        return *xstd::min_element(xstd::cbegin(orientations), xstd::cend(orientations), grid_less{g});
+        return *std::min_element(std::cbegin(orientations), std::cend(orientations), grid_less{g});
 }
 
 }       // namespace board
