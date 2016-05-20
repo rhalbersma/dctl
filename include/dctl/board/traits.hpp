e689eaaba10a7521d4acc4d8130c2e2e4b56e896
Rein Halbersma <Rein Halbersma@localhost>
2016-05-20 22:50:47 +0200

    add board traits to detect if pieces can be placed, moved and jumped

--- include/dctl/board/traits.hpp
+++ include/dctl/board/traits.hpp
@@ -1,16 +1,17 @@
 #pragma once
+#include <algorithm>    // min, max
 
 namespace dctl {
 namespace board {
 
 template<class Board>
-constexpr auto is_empty = (Board::width == 0 || Board::height == 0) || (Board::width == 1 && Board::height == 1 && Board::is_inverted);
+constexpr auto is_placeable = std::min(Board::width, Board::height) >= 1 && (!Board::is_inverted || std::max(Board::width, Board::height) > 1);
 
 template<class Board>
-constexpr auto is_moveable = Board::width >= 2 && Board::height >= 2;
+constexpr auto is_pushable = std::min(Board::width, Board::height) >= 2;
 
 template<class Board>
-constexpr auto is_jumpable = Board::width >= 3 && Board::height >= 3;
+constexpr auto is_jumpable = std::min(Board::width, Board::height) >= 3 && (!Board::is_inverted || std::max(Board::width, Board::height) > 3);
 
 }       // namespace board
 }       // namespace dctl
--- test/src/board/traits.cpp
+++ test/src/board/traits.cpp
@@ -1,20 +1,50 @@
-#include <dctl/board/traits.hpp>
-#include <dctl/board/rectangular.hpp>
+#include <board/sequence.hpp>                   // micro, mini, checkers, roman, spantsireti, international, frisian, ktar<10, 11>,
+                                                // ktar<10, 12>, compact_10_12, compact_12_10, rectangular<12, 10>, canadian, srilankan, dumm
+#include <dctl/board/rectangular.hpp>           // rectangular
+#include <dctl/board/traits.hpp>                // is_empty, is_pushable, is_jumpable
 #include <boost/test/test_case_template.hpp>    // BOOST_AUTO_TEST_CASE_TEMPLATE
-#include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_CHECK_EQUAL
+#include <boost/test/unit_test.hpp>             // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE
 
 namespace dctl {
 namespace board {
 
-BOOST_AUTO_TEST_SUITE(TestBoard)
+BOOST_AUTO_TEST_SUITE(BoardTraits)
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(Bit2SquareIsInverseSquare2Bit, T, BoardSequence)
+BOOST_AUTO_TEST_CASE(IsEmpty)
 {
-        BOOST_CHECK(
-                ranges::all_of(T::squares(), [](auto const sq) {
-                        return T::square_from_bit(T::bit_from_square(sq)) == sq;
-                })
-        );
+        static_assert(!is_placeable<rectangular<0, 0>>);
+        static_assert(!is_placeable<rectangular<0, 1>>);
+        static_assert(!is_placeable<rectangular<1, 0>>);
+        static_assert(!is_placeable<rectangular<1, 1, true>>);
+        static_assert( is_placeable<rectangular<1, 1>>);
+        static_assert( is_placeable<rectangular<2, 1, true>>);
+        static_assert( is_placeable<rectangular<1, 2, true>>);
+}
+
+BOOST_AUTO_TEST_CASE(IsPushable)
+{
+        static_assert(!is_pushable<rectangular<1, 1>>);
+        static_assert(!is_pushable<rectangular<2, 1>>);
+        static_assert(!is_pushable<rectangular<1, 2>>);
+        static_assert( is_pushable<rectangular<2, 2>>);
+}
+
+BOOST_AUTO_TEST_CASE(IsJumpable)
+{
+        static_assert(!is_jumpable<rectangular<2, 2>>);
+        static_assert(!is_jumpable<rectangular<3, 2>>);
+        static_assert(!is_jumpable<rectangular<2, 3>>);
+        static_assert(!is_jumpable<rectangular<3, 3, true>>);
+        static_assert( is_jumpable<rectangular<3, 3>>);
+        static_assert( is_jumpable<rectangular<4, 3, true>>);
+        static_assert( is_jumpable<rectangular<3, 4, true>>);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(IsRegular, T, BoardSequence)
+{
+        static_assert(is_placeable<T>);
+        static_assert(is_pushable<T>);
+        static_assert(is_jumpable<T>);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
