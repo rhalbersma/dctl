c3f5679e33f21d04dbc3238c6340a69f1408cb6b
rhalbersma <rhalbersma@gmail.com>
2016-11-02 18:18:12 +0100

    use boost.hana conventions for color/piece constants

--- include/dctl/actions/detail/detect_pawn_jump.hpp
+++ include/dctl/actions/detail/detect_pawn_jump.hpp
@@ -4,7 +4,7 @@
 #include <dctl/board/angle.hpp>                         // up, left_up, right_up, left, right, left_down, right_down, down
 #include <dctl/board/bearing.hpp>                       // bearing
 #include <dctl/board/mask/jump_targets.hpp>             // jump_targets
-#include <dctl/color_piece.hpp>                         // Color, color_constant, pawn_type
+#include <dctl/color_piece.hpp>                         // Color, color_constant, pawn_
 #include <dctl/rule_traits.hpp>                         // is_backward_pawn_jump, is_orthogonal_jump
 #include <dctl/utility/type_traits.hpp>                 // board_t, rules_t, set_t
 #include <type_traits>                                  // is_same
@@ -13,10 +13,11 @@ namespace dctl {
 namespace detail {
 
 template<Color Side, class Reverse, class State>
-class Detect<color_constant<Side>, pawn_type, select::jump, Reverse, State>
+class Detect<color_constant<Side>, pawn_, select::jump, Reverse, State>
 {
-        using color_type = color_constant<Side>;
-        using piece_type = pawn_type;
+        using to_move_ = color_constant<Side>;
+        static constexpr auto to_move_c = color_c<Side>;
+        static constexpr auto piece_c = pawn_c;
         using board_type = board_t<State>;
         using rules_type = rules_t<State>;
         using   set_type =   set_t<State>;
@@ -24,44 +25,44 @@ class Detect<color_constant<Side>, pawn_type, select::jump, Reverse, State>
         template<int Direction>
         using jump_targets = board::mask::jump_targets<board_type, Direction, short_ranged_tag>;
 
-        static constexpr auto orientation = board::bearing_v<board_type, color_type, Reverse>.degrees();
+        static constexpr auto orientation = board::bearing_v<board_type, to_move_, Reverse>.degrees();
 public:
         auto operator()(State const& state) const noexcept
         {
-                if (auto const sources = state.pieces(color_type{}, piece_type{}); sources.any()) {
-                        return directions(sources, state.pawn_targets(color_type{}), state.pieces(none_type{}));
+                if (auto const sources = state.pieces(to_move_c, piece_c); sources.any()) {
+                        return directions(sources, state.targets(to_move_c, piece_c), state.pieces(none_c));
                 }
                 return false;
         }
 private:
-        auto directions(set_type const sources, set_type const pawn_targets, set_type const not_occupied) const noexcept
+        auto directions(set_type const sources, set_type const targets, set_type const destinations) const noexcept
         {
                 if constexpr (!is_backward_pawn_jump_v<rules_type> && !is_orthogonal_jump_v<rules_type>) {
                         return directions_lfold<board::right_up, board::left_up>(
-                                sources, pawn_targets, not_occupied
+                                sources, targets, destinations
                         );
                 }
                 if constexpr (is_backward_pawn_jump_v<rules_type> && !is_orthogonal_jump_v<rules_type>) {
                         return directions_lfold<board::right_up, board::left_up, board::left_down, board::right_down>(
-                                sources, pawn_targets, not_occupied
+                                sources, targets, destinations
                         );
                 }
                 if constexpr (!is_backward_pawn_jump_v<rules_type> && is_orthogonal_jump_v<rules_type>) {
                         return directions_lfold<board::right, board::right_up, board::up, board::left_up, board::left>(
-                                sources, pawn_targets, not_occupied
+                                sources, targets, destinations
                         );
                 }
                 if constexpr (is_backward_pawn_jump_v<rules_type> && is_orthogonal_jump_v<rules_type>) {
                         return directions_lfold<board::right, board::right_up, board::up, board::left_up, board::left, board::left_down, board::down, board::right_down>(
-                                sources, pawn_targets, not_occupied
+                                sources, targets, destinations
                         );
                 }
         }
 
         template<template<int> class... Directions>
-        auto directions_lfold(set_type const sources, set_type const pawn_targets, set_type const not_occupied) const noexcept
+        auto directions_lfold(set_type const sources, set_type const targets, set_type const destinations) const noexcept
         {
-                return (... || jump_targets<Directions<orientation>{}>{}(sources, pawn_targets, not_occupied).any());
+                return (... || jump_targets<Directions<orientation>{}>{}(sources, targets, destinations).any());
         }
 };
 
