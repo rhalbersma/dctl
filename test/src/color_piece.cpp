dd52212c32d8ba7c5a521f8ffc8dc15b008935a0
Rein Halbersma <rhalbersma@gmail.com>
2016-11-21 23:59:25 +0100

    add unit tests for color_piece.hpp (closes #2)

---
+++ test/src/color_piece.cpp
@@ -0,0 +1,45 @@
+#include <dctl/color_piece.hpp>         //
+#include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE
+#include <type_traits>                  // is_same
+
+BOOST_AUTO_TEST_SUITE(ColorPiece)
+
+using namespace dctl;
+
+BOOST_AUTO_TEST_CASE(ColorsAreDifferent)
+{
+        static_assert(color::black != color::white);
+        static_assert(color::white != color::black);
+
+        static_assert(!std::is_same<black_, white_>{});
+        static_assert(!std::is_same<white_, black_>{});
+
+        static_assert(black_c != white_c);
+        static_assert(white_c != black_c);
+}
+
+BOOST_AUTO_TEST_CASE(ColorsAreNegativelyRelated)
+{
+        static_assert(!color::black == color::white);
+        static_assert(!color::white == color::black);
+
+        static_assert(std::is_same<opposite<black_>, white_>{});
+        static_assert(std::is_same<opposite<white_>, black_>{});
+
+        static_assert(!black_c == white_c);
+        static_assert(!white_c == black_c);
+}
+
+BOOST_AUTO_TEST_CASE(PiecesAreDifferent)
+{
+        static_assert(piece::pawn != piece::king);
+        static_assert(piece::king != piece::pawn);
+
+        static_assert(!std::is_same<pawn_, king_>{});
+        static_assert(!std::is_same<king_, pawn_>{});
+
+        static_assert(pawns_c != kings_c);
+        static_assert(kings_c != pawns_c);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
