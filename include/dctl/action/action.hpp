b1ee9f1e531cd1261f44af7bca1bf5e77edc9143
Rein Halbersma <rhalbersma@gmail.com>
2016-12-02 15:55:21 +0100

    fix build and all tests

--- include/dctl/action/action.hpp
+++ include/dctl/action/action.hpp
@@ -258,7 +258,7 @@ private:
 
         constexpr auto is_onboard(int const sq) const noexcept
         {
-                return sq < set_type::size();
+                return 0 <= sq && sq < set_type::size();
         }
 
         constexpr auto is_demotion() const noexcept
