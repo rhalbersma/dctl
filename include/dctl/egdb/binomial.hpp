326650b06c049ada6fc911e18fe95944afa90524
Rein Halbersma <rhalbersma@gmail.com>
2017-01-10 22:41:47 +0100

    document the binomial table initialization

--- include/dctl/egdb/binomial.hpp
+++ include/dctl/egdb/binomial.hpp
@@ -3,67 +3,99 @@
 #include <cassert>      // assert
 #include <cstddef>      // size_t
 #include <cstdint>      // int64_t
+#include <type_traits>  // is_integral
 
 namespace dctl {
 namespace egdb {
 namespace detail {
 
 // https://en.wikipedia.org/wiki/Binomial_coefficient
-template<int N, int K>
+template<int N, int K, class IntegralType>
 class binomial
 {
-        using table_type = std::array<std::array<int64_t, K + 1>, N + 1>;
-        static const table_type table;
-
-        // https://en.wikipedia.org/wiki/Pascal's_triangle
-        static auto pascal_triangle() noexcept
+        static constexpr auto static_assert_type_traits() noexcept
         {
-                table_type choose;
-
-                for (std::size_t n = 0; n < K; ++n) {
-                        for (std::size_t k = n + 1; k < K + 1; ++k) {
-                                choose[n][k] = 0LL;
-                        }
-                }
-                choose[0][0] = 1LL;
-                for (std::size_t n = 1; n < N + 1; ++n) {
-                        choose[n][0] = 1LL;
-                }
-                for (std::size_t n = 1; n < K + 1; ++n) {
-                        choose[n][n] = 1ULL;
-                }
-                for (std::size_t n = 1; n < K + 1; ++n) {
-                        for (std::size_t k = 1; k < n; ++k) {
-                                choose[n][k] = choose[n - 1][k - 1] + choose[n - 1][k];
-                        }
-                }
-                for (std::size_t n = K + 1; n < N + 1; ++n) {
-                        for (std::size_t k = 1; k < K + 1; ++k) {
-                                choose[n][k] = choose[n - 1][k - 1] + choose[n - 1][k];
-                        }
-                }
-                return choose;
+                static_assert(0 <= K); static_assert(K <= N);
+                static_assert(std::is_integral<IntegralType>{});
         }
+
+        using table_type = std::array<std::array<IntegralType, K + 1>, N + 1>;
+        static table_type const table;
+
 public:
         static auto coefficient(int n, int k) // Throws: Nothing.
-                -> int64_t
+                -> IntegralType
         {
                 if (k > n - k) {
                         k = n - k;
                 }
                 if (k < 0 || n < 0) {
-                        return 0LL;
+                        return 0;
                 }
                 assert(0 <= n); assert(n <= N);
                 assert(0 <= k); assert(k <= K);
                 assert(k <= n / 2);
                 return table[static_cast<std::size_t>(n)][static_cast<std::size_t>(k)];
         }
+
+private:
+        // https://en.wikipedia.org/wiki/Pascal's_triangle
+        static auto pascal_triangle() noexcept
+        {
+                table_type choose;
+
+                /*
+
+                               0 1 2 3 . K . . . . . N
+                             X-----------+-----------+
+                          0 /1\0 0 0 . 0/           / 0
+                         1 /1 1\0 0 . 0/           / 1
+                        2 /1 2 1\0 . 0/           / 2
+                       3 /1 3 3 1\. 0/           / 3
+                      . /. . . . .\0/           / .
+                     K /1 K . . K 1X           / K
+                    . /-----------/ \         / .
+                   . /.          /   \       / .
+                  . /.          /     \     / .
+                 . /.          /       \   / .
+                N /1 N . . . ./. . . N 1\ / N
+                 +-----------+-----------X
+                  0 1 2 3 . K . . . . . N
+
+                */
+
+                // upper-right triangle: width K, height K
+                for (std::size_t n = 0; n < K; ++n) {
+                        for (std::size_t k = n + 1; k < K + 1; ++k) {
+                                choose[n][k] = 0;
+                        }
+                }
+
+                // upper-left triangle: width K+1, height K+1
+                choose[0][0] = 1;
+                for (std::size_t n = 1; n < K + 1; ++n) {
+                        choose[n][0] = 1;
+                        for (std::size_t k = 1; k < n; ++k) {
+                                choose[n][k] = choose[n - 1][k - 1] + choose[n - 1][k];
+                        }
+                        choose[n][n] = 1;
+                }
+
+                // lower rectangle: width K+1, height N-K
+                for (std::size_t n = K + 1; n < N + 1; ++n) {
+                        choose[n][0] = 1;
+                        for (std::size_t k = 1; k < K + 1; ++k) {
+                                choose[n][k] = choose[n - 1][k - 1] + choose[n - 1][k];
+                        }
+                }
+
+                return choose;
+        }
 };
 
-template<int N, int K>
-const typename binomial<N, K>::table_type
-binomial<N, K>::table = binomial<N, K>::pascal_triangle();
+template<int N, int K, class IntegralType>
+typename binomial<N, K, IntegralType>::table_type const
+binomial<N, K, IntegralType>::table = binomial<N, K, IntegralType>::pascal_triangle();
 
 }       // namespace detail
 
@@ -74,8 +106,9 @@ auto choose(int const n, int const k) // Throws: Nothing.
         // For n <= 64: all k <= n/2 (e.g. Western chess board).
         // For n <= 90: all k <= 18  (e.g. Chinese chess board).
         static_assert((N <= 64 && K <= N / 2) || (N <= 90 && K <= 18));
+        assert(0 <= n); assert(n <= N);
 
-        return detail::binomial<N, K>::coefficient(n, k);
+        return detail::binomial<N, K, int64_t>::coefficient(n, k);
 }
 
 }       // namespace egdb
