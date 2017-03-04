5330d88ddd7e133559598ecc630775574bb571c5
Rein Halbersma <rhalbersma@gmail.com>
2017-03-04 22:22:11 +0100

    more formatting

--- include/dctl/utility/tti.hpp
+++ include/dctl/utility/tti.hpp
@@ -2,21 +2,22 @@
 #include <experimental/type_traits>     // void_t
 #include <type_traits>                  // conditional
 
-#define DCTL_PP_TTI_CONSTANT(NAME, DEFAULT)         \
-                                                    \
-template<class T, class = void>                     \
-static constexpr auto has_ ## NAME ## _v = false;   \
-                                                    \
-template<class T>                                   \
-static constexpr auto has_ ## NAME ## _v <          \
-    T, std::void_t<decltype(T::NAME)>               \
-> = true;                                           \
-                                                    \
-struct default_ ## NAME {                           \
-    static constexpr auto NAME = DEFAULT;           \
-};                                                  \
-                                                    \
-template<class T>                                   \
-constexpr auto NAME ## _v = std::conditional_t<     \
-    has_ ## NAME ## _v<T>, T, default_ ## NAME      \
->::NAME;                                            \
+#define DCTL_PP_TTI_CONSTANT(NAME, DEFAULT)             \
+                                                        \
+template<class T, class = void>                         \
+static constexpr auto has_ ## NAME ## _v = false;       \
+                                                        \
+template<class T>                                       \
+static constexpr auto has_ ## NAME ## _v<               \
+        T, std::void_t<decltype(T::NAME)>               \
+> = true;                                               \
+                                                        \
+struct default_ ## NAME                                 \
+{                                                       \
+        static constexpr auto NAME = DEFAULT;           \
+};                                                      \
+                                                        \
+template<class T>                                       \
+constexpr auto NAME ## _v = std::conditional_t<         \
+    has_ ## NAME ## _v<T>, T, default_ ## NAME          \
+>::NAME;                                                \
