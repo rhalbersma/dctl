213beb79ea5a8aaf83e9f4cc03cd82450df912ea
rhalbersma <rhalbersma@gmail.com>
2017-06-18 20:33:14 +0200

    [dxp] add test for invalid messages

--- test/src/core/state/dxp/parser.cpp
+++ test/src/core/state/dxp/parser.cpp
@@ -7,57 +7,63 @@
 
 using namespace dctl::core;
 
+factory
+<
+        dxp::game_request,
+        dxp::game_acknowledge,
+        dxp::move,
+        dxp::back_request,
+        dxp::back_acknowledge,
+        dxp::game_end,
+        dxp::chat
+> f;
+
 struct visitor
 {
-        std::vector<std::string>& vec;
+        std::vector<std::string>& data;
 
         template<class T>
-        auto operator()(T const& value) { vec.push_back(value.str()); }
+        auto operator()(T const& value) { data.push_back(value.str()); }
 
         auto operator()(std::monostate) {}
 };
 
+std::vector<std::string> parsed;
+auto vis = visitor{parsed};
+
 BOOST_AUTO_TEST_SUITE(DXPParser)
 
-BOOST_AUTO_TEST_CASE(MesanderMessageExamples)
-{
-        factory
-        <
-                dxp::game_request,
-                dxp::game_acknowledge,
-                dxp::move,
-                dxp::back_request,
-                dxp::back_acknowledge,
-                dxp::game_end,
-                dxp::chat
-        > f;
+	BOOST_AUTO_TEST_CASE(MesanderMessageExamples)
+	{
+		// Examples of DXP messages (Layer 2 protocol description)
+		// http://www.mesander.nl/damexchange/edxplg2.htm
+		std::vector<std::string> messages =
+		{
+		        "R01Tornado voor Windows 4.0        W060065A",
+		        "ATornado voor Windows 4.0        0",
+		        "M0012061100",
+		        "M001205250422122320",
+		        "B005Z",
+		        "K1",
+		        "E00",
+		        "CWhat do you think about move 35?"
+		};
 
-        // Examples of DXP messages (Layer 2 protocol description)
-        // http://www.mesander.nl/damexchange/edxplg2.htm
-        std::vector<std::string> messages =
-        {
-                "R01Tornado voor Windows 4.0        W060065A",
-                "ATornado voor Windows 4.0        0",
-                "M0012061100",
-                "M001205250422122320",
-                "B005Z",
-                "K1",
-                "E00",
-                "CWhat do you think about move 35?"
-        };
 
-        std::vector<std::string> parsed;
-        auto vis = visitor{parsed};
+		for (auto const& m : messages) {
+		        auto v = f.create(m);
+		        boost::apply_visitor(vis, v);
+		}
 
-        for (auto const& m : messages) {
-                auto v = f.create(m);
-                boost::apply_visitor(vis, v);
-        }
+		BOOST_CHECK_EQUAL_COLLECTIONS(messages.begin(), messages.end(), parsed.begin(), parsed.end());
+	}
 
-        BOOST_CHECK_EQUAL_COLLECTIONS(messages.begin(), messages.end(), parsed.begin(), parsed.end());
-
-	BOOST_CHECK_EQUAL(f.create("This is an invalid message").which(), 0);
-
-}
+	BOOST_AUTO_TEST_CASE(InvalidMessagesAreIgnored)
+	{
+		auto const n = vis.data.size();
+		auto v = f.create("This is an invalid message");
+		boost::apply_visitor(vis, v);
+		BOOST_CHECK_EQUAL(vis.data.size(), n);
+	}
 
 BOOST_AUTO_TEST_SUITE_END()
