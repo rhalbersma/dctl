0b2305f27a9fade843e91ec650b2a0f61120bbd6
rhalbersma <rhalbersma@gmail.com>
2017-08-07 23:47:53 +0200

    [actions] reorganized unit tests

--- test/src/core/actions/international.cpp
+++ test/src/core/actions/international.cpp
@@ -3,39 +3,46 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <core/actions/fixture.hpp>          // run
-#include <dctl/core/board.hpp>               // international
-#include <dctl/core/rules.hpp>               // international
+#include <core/actions/fixture.hpp>     // run
+#include <dctl/core/board.hpp>          // international
+#include <dctl/core/rules.hpp>          // international
 #include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_FIXTURE_TEST_CASE, BOOST_AUTO_TEST_SUITE_END
 #include <string>                       // string
 #include <vector>                       // vector
 
 using namespace dctl::core;
 
-using F = Fixture<international>;
+BOOST_AUTO_TEST_SUITE(ActionsInternational)
 
-BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
-
-        // Positions from the international rules (French tutorial):
-        // http://www.ffjd.fr/Web/index.php?page=reglesdujeu
+        using F = Fixture<international>;
 
         BOOST_AUTO_TEST_SUITE(FrenchTutorial)
 
-                BOOST_FIXTURE_TEST_CASE(Whitepawn_moveDirections, F)     // Art. 3.4
+                // Positions from the international rules (French tutorial):
+                // http://www.ffjd.fr/Web/index.php?page=reglesdujeu
+
+                BOOST_FIXTURE_TEST_CASE(WhitePawnMoveDirections, F)     // Art. 3.4
                 {
                         auto const fen = "W:W28";
                         auto const legal = std::vector<std::string>{ "28-22", "28-23" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(Blackpawn_moveDirections, F)     // Art. 3.4
+                BOOST_FIXTURE_TEST_CASE(BlackPawnMoveDirections, F)     // Art. 3.4
                 {
                         auto const fen = "B:B13";
                         auto const legal = std::vector<std::string>{ "13-18", "13-19" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(Whiteking_moveRange, F)          // Art. 3.9
+                BOOST_FIXTURE_TEST_CASE(PawnPromotion, F)               // Art. 3.5
+                {
+                        auto const fen = "W:W8";
+                        auto const legal = std::vector<std::string>{ "08-02", "08-03" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(WhiteKingMoveRange, F)          // Art. 3.9
                 {
                         auto const fen = "W:WK28";
                         auto const legal = std::vector<std::string>{
@@ -47,7 +54,7 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(Blackking_moveRange, F)          // Art. 3.9
+                BOOST_FIXTURE_TEST_CASE(BlackKingMoveRange, F)          // Art. 3.9
                 {
                         auto const fen = "B:BK1";
                         auto const legal = std::vector<std::string>{
@@ -58,14 +65,14 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(pawn_jump, F)                    // Art. 4.2
+                BOOST_FIXTURE_TEST_CASE(PawnJump, F)                    // Art. 4.2
                 {
                         auto const fen = "W:W28,32:B23";
                         auto const legal = std::vector<std::string>{ "28x19" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(king_jumpRange, F)               // Art. 4.3
+                BOOST_FIXTURE_TEST_CASE(KingJumpRange, F)               // Art. 4.3
                 {
                         auto const fen = "W:WK46:B23";
                         auto const legal = std::vector<std::string>{
@@ -74,14 +81,14 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(pawn_jumpContinuation, F)        // Art. 4.5
+                BOOST_FIXTURE_TEST_CASE(PawnJumpContinuation, F)        // Art. 4.5
                 {
                         auto const fen = "W:W15:B8,19,20";
                         auto const legal = std::vector<std::string>{ "15x02" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(king_jumpContinuation, F)        // Art. 4.6
+                BOOST_FIXTURE_TEST_CASE(KingJumpContinuation, F)        // Art. 4.6
                 {
                         auto const fen = "W:WK47:B9,12,33";
                         auto const legal = std::vector<std::string>{
@@ -90,14 +97,14 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(MultipleJumps, F)               // Art. 4.8
+                BOOST_FIXTURE_TEST_CASE(NoPassingJumpRemoval, F)        // Art. 4.8
                 {
                         auto const fen = "W:WK41,49:B9,10,12,19,38";
                         auto const legal = std::vector<std::string>{ "41x43" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(JumpPrecedence, F)              // Art. 4.13
+                BOOST_FIXTURE_TEST_CASE(QuantityPrecedence, F)          // Art. 4.13
                 {
                         auto const fen = "W:W48:B24,K31,34,K42,43";
                         auto const legal = std::vector<std::string>{ "48x19" };
@@ -106,12 +113,12 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
 
         BOOST_AUTO_TEST_SUITE_END()
 
-        // States from the official international rules (italian translation):
-        // http://www.fid.it/regolamenti/2008/RegTec_CAPO_II.pdf
+        BOOST_AUTO_TEST_SUITE(ItalianOfficialVersion)
 
-        BOOST_AUTO_TEST_SUITE(italianOfficialVersion)
+                // Positions from the official international rules (Italian translation):
+                // http://www.fid.it/regolamenti/2008/RegTec_CAPO_II.pdf
 
-                BOOST_FIXTURE_TEST_CASE(king_moveRange, F)               // Art. 3.9
+                BOOST_FIXTURE_TEST_CASE(KingMoveRange, F)               // Art. 3.9
                 {
                         auto const fen = "W:WK23";
                         auto const legal = std::vector<std::string>{
@@ -123,56 +130,56 @@ BOOST_AUTO_TEST_SUITE(ActionsRulebookinternational)
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(pawn_jumpDirections, F)          // Art. 4.2
+                BOOST_FIXTURE_TEST_CASE(PawnJumpDirections, F)          // Art. 4.2
                 {
                         auto const fen = "W:W35:B30,K40";
                         auto const legal = std::vector<std::string>{ "35x24", "35x44" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(king_jumpRange, F)               // Art. 4.3
+                BOOST_FIXTURE_TEST_CASE(KingJumpRange, F)               // Art. 4.3
                 {
                         auto const fen = "W:WK41:B23";
                         auto const legal = std::vector<std::string>{ "41x19", "41x14", "41x10", "41x05" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(pawn_jumpContinuation, F)        // Art. 4.5
+                BOOST_FIXTURE_TEST_CASE(PawnJumpContinuation, F)        // Art. 4.5
                 {
                         auto const fen = "W:W47:B13,14,22,24,31,34,K41,44";
                         auto const legal = std::vector<std::string>{ "47x49" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(king_jumpContinuation, F)        // Art. 4.6
+                BOOST_FIXTURE_TEST_CASE(KingJumpContinuation, F)        // Art. 4.6
                 {
                         auto const fen = "W:WK1:B7,9,17,19,20,30,31,33,43,44";
                         auto const legal = std::vector<std::string>{ "01x15" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(ApresFiniJumpRemoval, F)        // Art. 4.8
+                BOOST_FIXTURE_TEST_CASE(NoPassingJumpRemoval, F)        // Art. 4.8
                 {
                         auto const fen = "B:W27,28,38,39,42:BK25";
                         auto const legal = std::vector<std::string>{ "25x33" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(NumberOfpieces, F)              // Art. 4.13
+                BOOST_FIXTURE_TEST_CASE(QuantityPrecedence, F)          // Art. 4.13
                 {
                         auto const fen = "W:WK48:B7,8,31,34,K42,44";
                         auto const legal = std::vector<std::string>{ "48x50" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(NotNumberOfKings, F)            // Art. 4.14
+                BOOST_FIXTURE_TEST_CASE(NoContentsPrecedence, F)        // Art. 4.14
                 {
                         auto const fen = "W:W26:B12,K21,31,32";
                         auto const legal = std::vector<std::string>{ "26x08", "26x28" };
                         test(fen, legal);
                 }
 
-                BOOST_FIXTURE_TEST_CASE(ApresFinipromotion, F)          // Art. 4.15
+                BOOST_FIXTURE_TEST_CASE(NoPassingJumpPromotion, F)      // Art. 4.15
                 {
                         auto const fen = "W:W15:B9,10";
                         auto const legal = std::vector<std::string>{ "15x13" };
---
+++ test/src/core/actions/italian.cpp
@@ -0,0 +1,138 @@
+//          Copyright Rein Halbersma 2010-2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <core/actions/fixture.hpp>          // run
+#include <dctl/core/board.hpp>               // Roman
+#include <dctl/core/rules.hpp>               // italian
+#include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_FIXTURE_TEST_CASE, BOOST_AUTO_TEST_SUITE_END
+#include <string>                       // string
+#include <vector>                       // vector
+
+using namespace dctl::core;
+
+BOOST_AUTO_TEST_SUITE(ActionsItalian)
+
+        using F = Fixture<italian>;
+
+        BOOST_AUTO_TEST_SUITE(ItalianOfficialVersion)
+
+                // Positions from the official Italian rules:
+                // http://www.fid.it/regolamenti/2008/RegTec_CAPO_I.pdf
+
+                BOOST_FIXTURE_TEST_CASE(PawnJumpDirections, F)          // Art. 5.6
+                {
+                        auto const fen = "W:W30:B27";
+                        auto const legal = std::vector<std::string>{ "30x23" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(PawnJumpContinuation, F)        // Art. 5.7
+                {
+                        auto const fen = "W:W31:B12,20,28";
+                        auto const legal = std::vector<std::string>{ "31x08" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(KingJumpDirection, F)           // Art. 5.8
+                {
+                        auto const fen = "W:WK22:B18,19,27";
+                        auto const legal = std::vector<std::string>{ "22x13", "22x15", "22x31" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(KingJumpContinuation, F)        // Art. 5.9
+                {
+                        auto const fen = "W:WK27:B5,6,13,15,21,23";
+                        auto const legal = std::vector<std::string>{ "27x25" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(QuantityPrecedence, F)          // Art. 6.6
+                {
+                        auto const fen = "W:WK21,31:B10,18,27";
+                        auto const legal = std::vector<std::string>{ "31x06" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(ModalityPrecedence, F)          // Art. 6.7
+                {
+                        auto const fen = "W:WK23,31:B19,26,27";
+                        auto const legal = std::vector<std::string>{ "23x21" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(ContentsPrecedence, F)          // Art. 6.8
+                {
+                        auto const fen = "W:WK23:B19,K28";
+                        auto const legal = std::vector<std::string>{ "23x32" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(OrderingPrecedence, F)          // Art. 6.9
+                {
+                        auto const fen = "W:WK30:B10,12,18,K20,K26,27";
+                        auto const legal = std::vector<std::string>{ "30x05" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(EquivalentJumps, F)             // Art. 6.10
+                {
+                        auto const fen = "W:WK31:B18,20,K27,K28";
+                        auto const legal = std::vector<std::string>{ "31x13", "31x15" };
+                        test(fen, legal);
+                }
+
+        BOOST_AUTO_TEST_SUITE_END()
+
+        BOOST_AUTO_TEST_SUITE(AliotoDomenico)
+
+                // Suggested by Alioto Domenico in email to Ed Gilbert
+
+                BOOST_FIXTURE_TEST_CASE(OrderingPrecedence, F)          // Art. 6.9
+                {
+                        auto const fen = "W:WK30:B10,K12,K18,20,K26,K27";
+                        auto const legal = std::vector<std::string>{ "30x05" };
+                        test(fen, legal);
+                }
+
+        BOOST_AUTO_TEST_SUITE_END()
+
+        BOOST_AUTO_TEST_SUITE(JumpModality)
+
+                BOOST_FIXTURE_TEST_CASE(PawnsOnlyJumpForward, F)        // Art 5.3(b)
+                {
+                        auto const fen = "W:W24:B20,28";
+                        auto const legal = std::vector<std::string>{ "24x15" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(PawnsCannotJumpKings, F)        // Art 5.3(b)
+                {
+                        auto const fen = std::vector<std::string>{ "W:W30:BK26", "W:W30:B27" };
+                        auto const legal = std::vector<std::vector<std::string>>{ { "30-27" }, { "30x23" } };
+                        for (auto i = std::size_t{0}; i < fen.size(); ++i) {
+                                test(fen[i], legal[i]);
+                        }
+                }
+
+                BOOST_FIXTURE_TEST_CASE(KingsJumpForwardAndBackward, F) // Art 5.3(c)
+                {
+                        auto const fen = "W:WK24:B20,28";
+                        auto const legal = std::vector<std::string>{ "24x15", "24x31" };
+                        test(fen, legal);
+                }
+
+                BOOST_FIXTURE_TEST_CASE(KingsJumpPawnsAndKings, F)      // Art 5.3(d)
+                {
+                        auto const fen = std::vector<std::string>{ "W:WK30:BK26", "W:WK30:B27" };
+                        auto const legal = std::vector<std::vector<std::string>>{ { "30x21" }, { "30x23" } };
+                        for (auto i = std::size_t{0}; i < fen.size(); ++i) {
+                                test(fen[i], legal[i]);
+                        }
+                }
+
+        BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
