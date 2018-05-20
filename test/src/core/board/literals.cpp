ac520f9544c9b6fbc918860e008f2fb00c84cbd3
rhalbersma <rhalbersma@gmail.com>
2018-05-20 23:03:49 +0200

    [board] add more tests

--- test/src/core/board/literals.cpp
+++ test/src/core/board/literals.cpp
@@ -3,7 +3,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <dctl/core/board/angle.hpp>         // angle, _deg
+#include <dctl/core/board/angle.hpp>    // angle, _deg
 #include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE
 
 using namespace dctl::core;
