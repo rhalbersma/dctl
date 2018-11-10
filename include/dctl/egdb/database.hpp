95abd6e269413b380ef942ebf0c0a1bb1bcc90e2
rhalbersma <rhalbersma@gmail.com>
2018-11-10 21:50:56 +0100

    [egdb] use platform-dependent string type (fixes #25)

--- include/dctl/egdb/database.hpp
+++ include/dctl/egdb/database.hpp
@@ -50,7 +50,7 @@ public:
                                 std::copy(fs::directory_iterator(m_dir_path), fs::directory_iterator(), std::back_inserter(m_files));
                                 std::sort(m_files.begin(), m_files.end());
                                 std::cout << m_dir_path << " is a directory containing: " << m_files.size() << " files\n";
-                                std::copy(m_files.begin(), m_files.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+                                std::copy(m_files.begin(), m_files.end(), std::ostream_iterator<std::filesystem::path::string_type>(std::cout, "\n"));
                                 auto const s = static_cast<double>(std::accumulate(m_files.begin(), m_files.end(), 0ULL, [](auto sum, auto const& f) {
                                         return sum + fs::file_size(f);
                                 })) / static_cast<double>(1ULL << 30);
