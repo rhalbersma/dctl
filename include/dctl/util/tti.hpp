9d98597b2b5d02a59148234ebed95ebaa03c202d
rhalbersma <rhalbersma@gmail.com>
2022-09-27 15:53:47 +0200

    Use requires for type traits introspection

--- include/dctl/util/tti.hpp
+++ include/dctl/util/tti.hpp
@@ -5,25 +5,14 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <type_traits>  // conditional_t, void_t
-
-#define DCTL_PP_TTI_CONSTANT(NAME, DEFAULT)                     \
-                                                                \
-template<class T, class = void>                                 \
-inline constexpr static auto has_ ## NAME ## _v = false;        \
-                                                                \
-template<class T>                                               \
-inline constexpr static auto has_ ## NAME ## _v<                \
-        T, std::void_t<decltype(T::NAME)>                       \
-> = true;                                                       \
-                                                                \
-struct default_ ## NAME                                         \
-{                                                               \
-        constexpr static auto NAME = DEFAULT;                   \
-};                                                              \
-                                                                \
-template<class T>                                               \
-inline constexpr auto NAME ## _v = std::conditional_t<          \
-        has_ ## NAME ## _v<T>, T, default_ ## NAME              \
->::NAME;                                                        \
+#define DCTL_PP_TTI_CONSTANT(NAME, DEFAULT)     \
+                                                \
+template<class T>                               \
+inline constexpr auto NAME ## _v = []() {       \
+        if constexpr (requires { T::NAME; }) {  \
+            return T::NAME;                     \
+        } else {                                \
+            return DEFAULT;                     \
+        }                                       \
+}();                                            \
 
